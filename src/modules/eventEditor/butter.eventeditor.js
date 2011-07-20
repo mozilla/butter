9a78b6b3e58f2fa2f1851e607e1c55b3b3b2b7a5
Christopher De Cairos <chris.decairos@gmail.com>
2011-07-20 15:49:12 -0400

    fixes bugs. moves html out of src folder into test folder

--- src/modules/eventEditor/butter.eventeditor.js
+++ src/modules/eventEditor/butter.eventeditor.js
@@ -1,3 +1,27 @@
+/**********************************************************************************
+
+Copyright (C) 2011 by Mozilla Foundation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+**********************************************************************************/
+
 (function( window, document, undefined, Butter ) {
 
   Butter.registerModule( "eventeditor", (function() {
@@ -7,6 +31,8 @@
         binding,
         commServer,
         customEditors = {},
+        editorHeight,
+        editorWidth,
     
     constructEditor = function( trackEvent ) {
      
@@ -17,52 +43,57 @@
           commServer.send( "editorCommLink", trackEvent.popcornOptions, "updatetrackevent" );
         };
         
-      editorTarget && clearTarget()
+      editorTarget && clearTarget();
         
       if ( binding === "bindWindow" ) {
-        editorWindow = window.open( editorSrc, "", "width=400,height=400" );
+        editorWindow = window.open( editorSrc, "", "width=" + editorWidth + ",height=" + editorHeight + ",menubar=no,toolbar=no,location=no,status=no" );
+        setupServer();
         editorWindow.addEventListener( "unload", function() {
           butter.unlisten ( "trackeventupdated", updateEditor );
           butter.trigger( "trackeditforcedclosed" );
         }, false );
       } else if ( binding === "bindFrame" ) {
+        
         editorWindow = document.createElement( "iframe" );
+        setupServer();
         editorWindow.src = editorSrc;
         editorTarget.appendChild( editorWindow );
       }
       
-      commServer[ binding ]( "editorCommLink", editorWindow, function() {      
-        butter.listen( "trackeventupdated", updateEditor );
-        butter.listen( "targetadded", function() {
-          commServer.send( "editorCommLink", butter.getTargets(), "updatedomtargets" );
+      function setupServer() {
+        commServer[ binding ]( "editorCommLink", editorWindow, function() {      
+          butter.listen( "trackeventupdated", updateEditor );
+          butter.listen( "targetadded", function() {
+            commServer.send( "editorCommLink", butter.getTargets(), "updatedomtargets" );
+          });
+          commServer.listen( "editorCommLink", "okayclicked", function( newOptions ){
+            trackEvent.popcornOptions = newOptions;
+            editorWindow.close && editorWindow.close();
+            editorWindow && editorWindow.parentNode && editorWindow.parentNode.removeChild( editorWindow );
+            butter.unlisten ( "trackeventupdated", updateEditor );
+            butter.trigger( "trackeditclosed" );
+            butter.trigger( "trackeventupdated", trackEvent );
+          });
+          commServer.listen( "editorCommLink", "applyclicked", function( newOptions ) {
+            trackEvent.popcornOptions = newOptions;
+            butter.trigger( "trackeventupdated", trackEvent );
+          });
+          commServer.listen( "editorCommLink", "deleteclicked", function() {
+            butter.removeTrackEvent( trackEvent );
+            editorWindow.close && editorWindow.close();
+            editorWindow && editorWindow.parentNode && editorWindow.parentNode.removeChild( editorWindow );
+            butter.unlisten ( "trackeventupdated", updateEditor );
+            butter.trigger( "trackeditclosed" );
+          });
+          commServer.listen( "editorCommLink", "cancelclicked", function() {
+            editorWindow.close && editorWindow.close();
+            editorWindow && editorWindow.parentNode && editorWindow.parentNode.removeChild( editorWindow );
+            butter.unlisten ( "trackeventupdated", updateEditor );
+            butter.trigger( "trackeditclosed" );
+          });
+          commServer.send( "editorCommLink", { trackEvent: trackEvent, targets: butter.getTargets() }, "edittrackevent");
         });
-        commServer.listen( "editorCommLink", "okayclicked", function( newOptions ){
-          trackEvent.popcornOptions = newOptions;
-          editorWindow.close && editorWindow.close();
-          editorWindow && editorWindow.parentNode && removeChild( editorWindow );
-          butter.unlisten ( "trackeventupdated", updateEditor );
-          butter.trigger( "trackeditclosed" );
-          butter.trigger( "trackeventupdated" );
-        });
-        commServer.listen( "editorCommLink", "applyclicked", function( newOptions ) {
-          trackEvent.popcornOptions = newOptions;
-          butter.trigger( "trackeventupdated" );
-        });
-        commServer.listen( "editorCommLink", "deleteclicked", function() {
-          butter.removeTrackEvent( trackEvent );
-          editorWindow.close && editorWindow.close();
-          editorWindow && editorWindow.parentNode && removeChild( editorWindow );
-          butter.unlisten ( "trackeventupdated", updateEditor );
-          butter.trigger( "trackeditclosed" );
-        });
-        commServer.listen( "editorCommLink", "cancelclicked", function() {
-          editorWindow.close && editorWindow.close();
-          editorWindow && editorWindow.parentNode && removeChild( editorWindow );
-          butter.unlisten ( "trackeventupdated", updateEditor );
-          butter.trigger( "trackeditclosed" );
-        });
-        commServer.send( "editorCommLink", { trackEvent: trackEvent, targets: butter.getTargets() }, "edittrackevent");
-      });
+      }
       
     },
 
@@ -77,6 +108,12 @@
     
       setup: function( options ) {
 
+        if ( options ) { 
+        
+          editorWidth = options.editorWidth || 400;
+          editorHeight = options.editorHeight || 400;
+        } 
+        
         if ( options && options.target && typeof options.target === "string" ) {
 
           editorTarget = document.getElementById( options.target ) || {};
