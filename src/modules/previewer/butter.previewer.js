685d3ca031ed7988e3cb9259d5ae67333e9f072b
David Seifried <david.c.seifried@gmail.com>
2011-08-02 10:55:47 -0400

    Removed a console.log and one line tabbed too far

--- src/modules/previewer/butter.previewer.js
+++ src/modules/previewer/butter.previewer.js
@@ -127,7 +127,7 @@
       buildPopcorn: function( videoTarget, callback ) {
 
         videoURL = this.getCurrentMedia().getMedia();
-        console.log("VIDJA TARGET", videoTarget.getMedia());
+
         // default to first butter-media tagged object if none is specified
         videoTarget = videoTarget.getName();
 
@@ -172,7 +172,7 @@
         players[ undefined ] = function() {
           var src = document.createElement( "source" ),
               video = document.createElement( "video" );
-              src.src = videoURL;
+          src.src = videoURL;
 
           video.style.width = videoTarget.width;
           video.style.height = videoTarget.height;
