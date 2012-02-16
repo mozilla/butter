c848ea5615971a8c2b751759a0341271a6710868
David Seifried <david.c.seifried@gmail.com>
2012-02-16 14:29:33 -0500

    [#368] Changed from target.object being an id to target.object being the html element

--- src/core/target.js
+++ src/core/target.js
@@ -32,7 +32,7 @@ THE SOFTWARE.
 
       options = options || {};
       var name = options.name || "Target" + id + Date.now();
-      this.object = options.object;
+      this.object = document.getElementById( options.object );
 
       Object.defineProperty( this, "name", {
         get: function() {
@@ -48,13 +48,7 @@ THE SOFTWARE.
 
       Object.defineProperty( this, "json", {
         get: function() {
-          var obj;
-          try {
-            obj = JSON.stringify( this.object );
-          }
-          catch ( e ) {
-            obj = this.object.toString();
-          }
+          var obj = this.object.id ? this.object.id.toString() : "";
           return {
             id: id,
             name: name,
@@ -63,9 +57,9 @@ THE SOFTWARE.
         },
         set: function( importData ) {
           if ( importData.name ) {
-            name = importData.name
+            name = importData.name;
           }
-          this.object = importData.object
+          this.object = document.getElementById( importData.object );
         }
       });
     }; //Target
