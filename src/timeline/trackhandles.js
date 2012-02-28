cfab4b901da4c3d4bc702d7cfca7e7cba49db7e2
Bobby Richter <secretrobotron@gmail.com>
2012-02-28 09:40:45 -0500

    Playing with different UI elements for Track Editor UI.

--- src/timeline/trackhandles.js
+++ src/timeline/trackhandles.js
@@ -6,6 +6,7 @@ define( [], function(){
         _container = document.createElement( "div" ),
         _list = document.createElement( "div" ),
         _tracks = {},
+        _menus = [],
         _this = this;
 
     _container.className = "track-handle-container";
@@ -19,6 +20,9 @@ define( [], function(){
       start: function( e, ui ){
         // put this first to make ordering sane
         _list.insertBefore( ui.item[ 0 ], _list.firstChild );
+        for( var i=0, l=_menus.length; i<l; ++i ){
+          _menus[ i ].style.display = "none";
+        } //for
       },
       change: function( e, ui ){
 
@@ -37,26 +41,49 @@ define( [], function(){
           } //if
         } //for
 
-      } //change
+      }, //change
+      stop: function(){
+        for( var i=0, l=_menus.length; i<l; ++i ){
+          _menus[ i ].style.display = "block";
+        } //for
+      }
     });
 
     _media.listen( "trackadded", function( e ){
       var trackId = e.data.id,
-          trackDiv = document.createElement( "div" );
+          trackDiv = document.createElement( "div" ),
+          menuDiv = document.createElement( "div" ),
+          deleteButton = document.createElement( "div" ),
+          editButton = document.createElement( "div" );
+
+      menuDiv.className = "menu";
+      deleteButton.className = "delete";
+      //editButton.className = "edit";
+
+      //menuDiv.appendChild( editButton );
+      menuDiv.appendChild( deleteButton );
+
+      _menus.push( menuDiv );
+
       trackDiv.className = "track-handle";
       trackDiv.id = "track-handle-" + trackId;
       trackDiv.appendChild( document.createTextNode( e.data.name ) );
+      trackDiv.appendChild( menuDiv );
+
       _list.appendChild( trackDiv );
+
       _tracks[ trackId ] = {
         id: trackId,
         track: e.data,
-        element: trackDiv
+        element: trackDiv,
+        menu: menuDiv
       };
     });
 
     _media.listen( "trackremoved", function( e ){
       var trackId = e.data.id;
       _list.removeChild( _tracks[ trackId ].element );
+      _menus.splice( _menus.indexOf( _tracks[ trackId ].menu ), 1 );
       delete _tracks[ trackId ];
     });
 
