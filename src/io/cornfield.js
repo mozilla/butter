e098fee83e765ee0bd21b2541585b5e5450c7da4
Jon Buckley <jon@jbuckley.ca>
2012-02-29 17:33:32 -0500

    Add QUnit tests for Cornfield

--- src/io/cornfield.js
+++ src/io/cornfield.js
@@ -43,20 +43,19 @@ THE SOFTWARE.
     var Cornfield = function( butter, config ) {
 
       var email = "",
-          server = config.server,
-          verifier = server + "/browserid/verify";
+          server = config.server;
 
-      this.authorize = function(callback) {
+      this.login = function(callback) {
         navigator.id.get(function(assertion) {
           if (assertion) {
-            XHR.post(verifier,
+            XHR.post(server + "/browserid/verify",
               { audience: audience(), assertion: assertion },
               function() {
                 if (this.readyState === 4) {
                   var response = JSON.parse(this.response);
                   if (response.status === "okay") {
                     email = response.email;
-                    callback(email);
+                    callback(response);
                   } else {
                     callback(undefined);
                   }
@@ -72,6 +71,15 @@ THE SOFTWARE.
         return email;
       };
 
+      this.logout = function(callback) {
+        XHR.get(server + "/browserid/logout", function() {
+          if (this.readyState === 4) {
+            var response = JSON.parse(this.response);
+            callback(response);
+          }
+        });
+      }
+
       this.list = function(callback) {
         XHR.get(server + "/files", function() {
           if (this.readyState === 4) {
@@ -84,8 +92,7 @@ THE SOFTWARE.
       this.pull = function(name, callback) {
         XHR.get(server + "/files/" + name, function() {
           if (this.readyState === 4) {
-            var response = JSON.parse(this.response);
-            callback(response);
+            callback(this.response);
           }
         });
       };
