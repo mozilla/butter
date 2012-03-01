1714ef7e9cbd93a93716204579836b447714af16
Bobby Richter <secretrobotron@gmail.com>
2012-03-01 17:22:47 -0500

    Fixed license url in test/core.js.

--- test/core.js
+++ test/core.js
@@ -2,7 +2,7 @@
 
 /* This Source Code Form is subject to the terms of the MIT license
  * If a copy of the MIT license was not distributed with this file, you can
- * obtain one at http://www.opensource.org/licenses/mit-license.php */
+ * obtain one at http://www.mozillapopcorn.org/butter-license.txt */
 
 (function(window, document, undefined ){
 
