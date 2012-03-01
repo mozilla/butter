8e4120d0e3c97a46d9203a324fefc26a5336a53e
Bobby Richter <secretrobotron@gmail.com>
2012-03-01 14:10:39 -0500

    Removed copyright from license headers.

--- src/timeline/trackliner/track-view.js
+++ src/timeline/trackliner/track-view.js
@@ -1,5 +1,4 @@
-/* Copyright 2011, 2012 - Mozilla Foundation
- * This Source Code Form is subject to the terms of the MIT license
+/* This Source Code Form is subject to the terms of the MIT license
  * If a copy of the MIT license was not distributed with this file, you can
  * obtain one at http://www.opensource.org/licenses/mit-license.php */
 
