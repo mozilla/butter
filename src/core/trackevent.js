2d6a3d074c63730ab3f794ddc49c8c5ab4bbd027
Bobby Richter <secretrobotron@gmail.com>
2012-03-01 13:59:18 -0500

    Fixed MPL -> MIT

--- src/core/trackevent.js
+++ src/core/trackevent.js
@@ -1,6 +1,6 @@
 /* Copyright 2011, 2012 - Mozilla Foundation
  * This Source Code Form is subject to the terms of the MIT license
- * If a copy of the MPL was not distributed with this file, you can
+ * If a copy of the MIT license was not distributed with this file, you can
  * obtain one at http://www.opensource.org/licenses/mit-license.php */
 
 define( [ "./logger", "./eventmanager", "util/lang" ], function( Logger, EventManager, util ) {
