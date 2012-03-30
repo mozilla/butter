a8908063d2be09e90cf58244e8f27ae190dbd3ae
David Seifried <david.c.seifried@gmail.com>
2012-03-30 17:50:39 -0400

    [#457] Destroyed the console.log

--- src/core/popcorn-wrapper.js
+++ src/core/popcorn-wrapper.js
@@ -83,7 +83,6 @@ define( [ "core/logger", "core/eventmanager" ], function( Logger, EventManager )
       } //failureWrapper
       function popcornSuccess( e ){
          addPopcornHandlers();
-         console.log(_onPrepare);
         _onPrepare( e );
       } //popcornSuccess
       findMediaType( url );
