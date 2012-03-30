fb7bfebfbbd87d3976d153906cd52260912eac0e
secretrobotron <secretrobotron@gmail.com>
2012-03-30 10:21:11 -0400

    [t467] - Elegant tray loading
    1. Added spinning logo.
    2. Tray comes up once "ready" signal is emitted from core.

---
+++ src/ui/logo-spinner.js
@@ -0,0 +1,35 @@
+define([], function(){
+  
+  return function( parentElement ){
+
+    var outerElement = document.createElement( "div" ),
+        innerElement = document.createElement( "div" );
+
+    outerElement.className = "butter-logo-spin-outer";
+    innerElement.className = "butter-logo-spin-inner";
+
+    outerElement.appendChild( innerElement );
+
+    if( parentElement ){
+      parentElement.appendChild( outerElement );
+    }
+
+    return {
+      element: outerElement,
+      start: function(){
+        innerElement.classList.add( "active" );
+      },
+      stop: function(){
+        innerElement.classList.remove( "active" );
+      },
+      show: function(){
+        outerElement.classList.remove( "fade-out" );
+      },
+      hide: function(){
+        outerElement.classList.add( "fade-out" );
+      }
+    };
+
+  };
+
+})
