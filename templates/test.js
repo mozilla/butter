75cee282556737185585e33911cfd2c6ad255bac
Kate Hudson <katehudson@loaner-02177.local>
2012-04-04 17:06:15 -0400

    Added all plugins to complete.conf

--- templates/test.js
+++ templates/test.js
@@ -1,7 +1,7 @@
 document.addEventListener( "DOMContentLoaded", function( e ){
 
   Butter({
-    config: "../config/default.conf",
+    config: "../config/complete.conf",
     ready: function( butter ){
       var media = butter.media[ 0 ];
 
