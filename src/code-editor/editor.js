5719172d6f149007e56d2854b3a2fed73e1e7867
secretrobotron <secretrobotron@gmail.com>
2012-05-15 17:38:55 -0400

    [t363] CodeMirror added

--- src/code-editor/editor.js
+++ src/code-editor/editor.js
@@ -2,8 +2,15 @@
  * If a copy of the MIT license was not distributed with this file, you can
  * obtain one at http://www.mozillapopcorn.org/butter-license.txt */
 
-define([ 'codemirror' ], function( CodeMirror ){
+define([ 'codemirror' ], function(){
 
-  console.log(CodeMirror);
-
+  setTimeout(function(){
+  var myCodeMirror = CodeMirror(function(element){
+      document.body.appendChild(element);
+    },
+    {
+      value: document.body.innerHTML,
+      mode: "javascript"
+    });
+  }, 2000);
 });
