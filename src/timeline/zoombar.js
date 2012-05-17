a93efcbc8cc7f720cf066dd29e6cc4590e447542
Scott Downe <scott.downe@gmail.com>
2012-05-17 12:41:27 -0400

    [#435] removed comment.

--- src/timeline/zoombar.js
+++ src/timeline/zoombar.js
@@ -54,7 +54,7 @@ define( [], function(){
       _handle.style.width = _handleWidth + "px";
       _elementWidth = _rect.width;
       if( level !== undefined ){
-        _handle.style.left = ( _rect.width - _handle.clientWidth ) * level + "px"; //( level * _handleWidth ) + "px";
+        _handle.style.left = ( _rect.width - _handle.clientWidth ) * level + "px";
         zoomCallback( _handle.offsetLeft / ( _rect.width - _handle.clientWidth ) );
       } //if
     };
