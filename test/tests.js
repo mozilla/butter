1fb2ddecd81aebb2d92dce41571079888b364d4b
Matthew Schranz <schranz.m@gmail.com>
2012-05-18 14:56:47 -0400

    [#1192] Added Butter Test Runner. All QUnit Tests now run the butter.all.unit.html page

---
+++ test/tests.js
@@ -0,0 +1,19 @@
+var Butter_tests = [{
+  name: "Core",
+  path: "./index.html",
+  type: "Core"
+}, {
+  name: "Event Manager",
+  path: "eventmanager/index.html",
+  type: "Event"
+}, {
+  name: "Cornfield",
+  path: "cornfield/index.html",
+  type: "Cornfield"
+}, {
+  name: "Plugin Manager",
+  path: "pluginmanager/index.html",
+  type: "Plugin"
+}
+
+];
