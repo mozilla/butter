331ea5c7e9cdca2c942f9f99e72bac0ff7f9cf06
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2012-05-25 17:07:01 -0400

    Fix type on 'destroy' as per review

--- src/core/popcorn-wrapper.js
+++ src/core/popcorn-wrapper.js
@@ -403,7 +403,7 @@ define( [ "core/logger", "core/eventmanager" ], function( Logger, EventManager )
      */
     function waitForMedia( readyCallback, timeoutCallback ){
       checkTimeoutLoop(function(){
-        return ( _popcorn && /* Make sure _popcorn still exists (e.g., destory() hasn't been called) */
+        return ( _popcorn && /* Make sure _popcorn still exists (e.g., destroy() hasn't been called) */
                  ( _popcorn.media.readyState >= 1 &&
                    _popcorn.duration() > 0
                  )
