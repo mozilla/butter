148fb2672391eff829298355e45f542113d23426
Kate Hudson <katehudsondesign@gmail.com>
2012-05-31 13:09:26 -0400

    added normalise, testing styles

--- templates/commentary/scripts.js
+++ templates/commentary/scripts.js
@@ -6,52 +6,19 @@ document.addEventListener( "DOMContentLoaded", function( e ){
       var media = butter.media[ 0 ];
 
       function start(){
-        var track = media.addTrack( "Track1" );
-        media.addTrack( "Track" + Math.random() );
-        media.addTrack( "Track" + Math.random() );
-
-        var event = track.addTrackEvent({
-          type: "text",
-          popcornOptions: {
-            start: 0,
-            end: 1,
-            text: "test",
-            target: "video-overlay"
-          }
-        });
+        media.addTrack( "Track1" );
+        media.addTrack( "Track2" );
+        media.addTrack( "Track3" );
 
         butter.tracks[ 0 ].addTrackEvent({ 
           type: "text",
           popcornOptions: {
-            start: 1,
+            start: 0,
             end: 2,
             text: "Test",   
             target: "sidebar"
           }
         });
-    
-        butter.tracks[ 0 ].addTrackEvent({ 
-          type: "webpage",
-          popcornOptions: {
-            start:2,
-            end: 3,
-            id:"webpages-a",
-            src:"http://popcornjs.org/",
-            target: "sidebar"
-          }
-        });
-
-        butter.tracks[ 0 ].addTrackEvent({ 
-          type: "wikipedia",
-          popcornOptions: {
-            start:3,
-            end: 4,
-            title: "Cape Town yo",
-            src:"http://en.wikipedia.org/wiki/Cape_Town",
-            target: "sidebar"
-          }
-        });
-
 
       }
 
