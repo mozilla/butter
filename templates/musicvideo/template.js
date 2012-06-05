39dd2091f8e4774ec2c3b4d062d7723dc201b880
Kate Hudson <katehudsondesign@gmail.com>
2012-06-05 17:12:52 -0400

    added all templates to template directory

---
+++ templates/musicvideo/template.js
@@ -0,0 +1,63 @@
+document.addEventListener( "DOMContentLoaded", function( e ){
+
+  Butter({
+    config: "config.json",
+    ready: function( butter ){
+      var media = butter.media[ 0 ];
+
+      function start(){
+        var track = media.addTrack( "Track1" );
+        media.addTrack( "Track" + Math.random() );
+
+        var event = track.addTrackEvent({
+          type: "text",
+          popcornOptions: {
+            start: 0,
+            end: 3,
+            text: "Mother Mother - Hayloft",
+            target: "Title"
+          }
+        });
+        
+         butter.tracks[ 0 ].addTrackEvent({
+            type: "text",
+            popcornOptions: {
+               start: 4,
+               end: 9,
+               text: "  Luxury Wafers Sessions",
+               target: "Title"
+             }
+           });
+           
+        butter.tracks[ 1 ].addTrackEvent({
+          type: "lastfm",
+          popcornOptions: {
+             start: 1  ,
+             end: 6,
+             artist: "mother mother",
+             apikey: "30ac38340e8be75f9268727cb4526b3d",
+             target: "Area1"
+           }
+         });
+         
+         butter.tracks[ 1 ].addTrackEvent({
+            type: "flickr",
+             popcornOptions: {
+               start: 6,
+               end: 9 ,
+               height: 120,
+               width: 120,
+               tags: "mothermother",
+               numberofimages: 10,
+               target: "Area1"
+             }
+           });
+
+      }
+
+      window.butter = butter;
+      media.onReady( start );
+
+    }
+  }); //Butter
+}, false );
---
+++ templates/newscaster/js/script.js
@@ -0,0 +1,21 @@
+/* Author:
+
+*/
+
+
+
+/* Mustache:
+//Add .mustache-template to your templates.
+var person = {
+    firstName: "Christophe",
+    lastName: "Coenraets",
+    blogURL: "http://coenraets.org"
+};
+
+var template = document.getElementById("template").innerHTML;
+var html = Mustache.to_html(template, person);
+document.getElementById("sampleArea").innerHTML = html;
+*/
+
+
+
