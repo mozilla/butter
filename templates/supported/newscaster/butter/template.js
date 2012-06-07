c24060252b6e09f63bf1c364339629c11ef3b907
Kate Hudson <katehudsondesign@gmail.com>
2012-06-07 15:40:51 -0400

    added image2 draggable hook

--- templates/supported/newscaster/butter/template.js
+++ templates/supported/newscaster/butter/template.js
@@ -6,7 +6,8 @@ document.addEventListener( "DOMContentLoaded", function( e ){
   Butter({
     config: "butter/config.json",
     ready: function( butter ){
-      var media = butter.media[ 0 ];
+      var media = butter.media[ 0 ],
+          popcorn = butter.media[ 0 ].popcorn.popcorn;
 
       function start(){
         media.addTrack( "Track1" );
@@ -23,6 +24,101 @@ document.addEventListener( "DOMContentLoaded", function( e ){
           }
         });
 
+      // EDITING HOOKS
+      // Would be good if these were part of a library in shared, instead of existing here.
+      // Should be updated to a hook syntax like hook("image2", myUpdateFunction) or something
+      // Add listeners for future track events.
+        butter.listen("trackeventadded", updateFunction);
+        butter.listen("trackeventupdated", updateFunction);
+
+      function updateFunction(e) {
+
+        var trackEvent,
+            _container = null,
+            _textEls;
+
+        if (e.type==="trackeventadded") { 
+          trackEvent = e.data; 
+        } else if (e.type==="trackeventupdated") { 
+          trackEvent = e.target; 
+        } else { trackEvent = e; }
+
+        // Requires _container in options to work.
+        trackEvent.popcornTrackEvent = popcorn.getTrackEvent( popcorn.getLastTrackEventId() ); //Store a reference
+        _container = trackEvent.popcornTrackEvent._container;
+        if (!_container ) { return; }
+
+        if( trackEvent.type === "image2" ) {
+          trackEvent.popcornTrackEvent._image.addEventListener("mousedown", function(e){
+            e.preventDefault();
+          }, false);
+          window.$ && $( _container ).resizable({
+            start: function(event, ui) {
+              _container.style.border = "2px dashed #CCC";
+            },
+            stop: function(event, ui) {
+              _container.style.border = "";
+              trackEvent.update({ height: ui.size.height + "px", width: ui.size.width + "px" })
+            }
+          }).draggable({
+            stop: function(event, ui) {
+                trackEvent.update({top: ui.position.top + "px", left: ui.position.left + "px" });
+            }
+          });
+
+          /* Drag and drop DataURI */
+          var canvas = document.createElement( "canvas" ),
+              context,
+              dropTarget,
+              field;
+
+          canvas.id = "grabimage";
+          canvas.style.display = "none";
+
+          dropTarget = _container;
+
+          dropTarget.addEventListener( "dragover", function( event ) {
+            event.preventDefault();
+            dropTarget.className = "dragover";
+          }, false);
+
+          dropTarget.addEventListener( "dragleave", function( event ) {
+            event.preventDefault();
+            dropTarget.className = "";
+          }, false);
+
+          dropTarget.addEventListener( 'drop', function( event ) {
+            dropTarget.className = "dropped";
+            event.preventDefault();
+            var file = event.dataTransfer.files[ 0 ],
+                imgSrc,
+                image,
+                imgURI;
+
+            if( !file ) { return; }
+
+            if( window.URL ) { 
+              imgSrc = window.URL.createObjectURL( file );
+            } else if ( window.webkitURL ) {
+              imgSrc = window.webkitURL.createObjectURL( file );
+            }
+
+            image = document.createElement( 'img' );
+            image.onload = function () {
+                canvas.width = this.width;
+                canvas.height = this.height;
+                context = canvas.getContext( '2d' );
+                context.drawImage( this, 0, 0, this.width, this.height );
+                imgURI = canvas.toDataURL();  
+                trackEvent.update( {"src" : imgURI } );
+            };
+            image.src = imgSrc;
+        
+          }, false);
+
+        }
+      }
+  
       } //start
 
       media.onReady( start );
