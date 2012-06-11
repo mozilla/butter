16d0d591d4f8f7b613938d7402b0b3defc6ae50b
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2012-06-11 12:06:03 -0400

    Move /*! to proper comment block so it doesn't get wiped out by uglify
    Conflicts:
    
    	src/util/lang.js

--- src/butter.js
+++ src/butter.js
@@ -1,4 +1,4 @@
-/* This Source Code Form is subject to the terms of the MIT license
+/*! This Source Code Form is subject to the terms of the MIT license
  * If a copy of the MIT license was not distributed with this file, you can
  * obtain one at http://www.mozillapopcorn.org/butter-license.txt */
 
