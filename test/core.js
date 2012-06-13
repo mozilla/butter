468abd293f504ca42fb865bc207e29afdacdb8fd
secretrobotron <secretrobotron@gmail.com>
2012-06-13 11:32:19 -0400

    [t1357] updated popcorn, tests for functionality

--- test/core.js
+++ test/core.js
@@ -221,6 +221,44 @@
     });
   });
 
+  asyncTest( "Media targets", 1, function(){
+    var popcornTextPlugin = document.createElement( "script" );
+    popcornTextPlugin.src = "../external/popcorn-js/plugins/text/popcorn.text.js";
+    document.body.appendChild( popcornTextPlugin );
+
+    var videoDiv = document.createElement( "video" );
+    videoDiv.id = "media-target-test-div";
+    videoDiv.setAttribute( "data-butter", "media" );
+    videoDiv.setAttribute( "src", "../external/popcorn-js/test/trailer.ogv" );
+    document.body.appendChild( videoDiv );
+
+    createButter( function( butter ) {
+      butter.config.value( "scrapePage", true );
+      butter.preparePage(function(){
+        var media = butter.media[0];
+
+        media.listen( "mediaready", function(){
+          var trackEvent = media.addTrack().addTrackEvent({
+                type: "text",
+                popcornOptions: {
+                  start: 1,
+                  end: 2,
+                  text: "LOL",
+                  target: "media-target-test-div"
+                }
+              });
+          media.currentTime = 1.5;
+          var contentDiv = document.getElementById( "media-target-test-div-overlay" );
+          ok( contentDiv, contentDiv.innerHTML === "LOL", "Media has target div with correct content." );
+          document.body.removeChild( videoDiv );
+          start();
+        });
+      });
+      //
+    });
+
+  });
+
   module( "Track", butterLifeCycle );
 
   asyncTest( "Create Track object", 1, function(){
