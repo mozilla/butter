afce18fcc4433d08c553dcc02e1d45b3c4b94eac
David Humphrey <david.humphrey@senecac.on.ca>
2012-06-14 14:08:36 -0400

    [t1540] Fix lint issue in src/main.js

--- src/main.js
+++ src/main.js
@@ -703,7 +703,7 @@
           var xhr = new XMLHttpRequest(),
               savedData;
 
-          savedDataUrl += "?noCache=" + Date.now(),
+          savedDataUrl += "?noCache=" + Date.now();
 
           xhr.open( "GET", savedDataUrl, false );
 
