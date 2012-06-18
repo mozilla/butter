922352c36cd3ebddd0a58e47e2e2944f0a04ea30
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2012-06-18 19:09:00 -0400

    [t1621] Handle options going away with speak worker

--- templates/supported/shared/plugins/speak/popcorn.speak.js
+++ templates/supported/shared/plugins/speak/popcorn.speak.js
@@ -189,6 +189,12 @@
               var p = popcornInstances[ event.data.popcornID ],
                   options = p.getTrackEvent( event.data.optionsID );
 
+              // Bail if options object has vanished since this
+              // request was originally made to build the speech.
+              if( !options ){
+                return;
+              }
+
               // Build Audio element and put on options object.
               handleWav( options, event.data.data );
             };
