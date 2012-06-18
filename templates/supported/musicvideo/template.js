4fa480966e505cad32c46e9230622db6bd7d5866
Jon Buckley <jon@jbuckley.ca>
2012-06-18 19:07:57 -0400

    Move default data to saved-data.json [#1429]

--- templates/supported/musicvideo/template.js
+++ templates/supported/musicvideo/template.js
@@ -4,61 +4,6 @@
 document.addEventListener( "DOMContentLoaded", function( e ){
 
   Butter({
-    config: "config.json",
-    ready: function( butter ){
-      var media = butter.currentMedia;
-
-      function start(){
-        var track1 = media.addTrack( "Track1" ),
-            track2 = media.addTrack();
-
-        track1.addTrackEvent({
-          type: "text",
-          popcornOptions: {
-            start: 0,
-            end: 3,
-            text: "Music video",
-            target: "Title"
-          }
-        });
-        
-        track1.addTrackEvent({
-          type: "text",
-          popcornOptions: {
-            start: 4,
-            end: 9,
-            text: "Cool!",
-            target: "Title"
-           }
-        });
-           
-        track2.addTrackEvent({
-          type: "lastfm",
-          popcornOptions: {
-            start: 1,
-            end: 6,
-            artist: "mother mother",
-            apikey: "30ac38340e8be75f9268727cb4526b3d",
-            target: "Area1"
-           }
-        });
-         
-        track1.addTrackEvent({
-          type: "flickr",
-          popcornOptions: {
-            start: 6,
-            end: 9 ,
-            height: 120,
-            width: 120,
-            tags: "mothermother",
-            numberofimages: 10,
-            target: "Area1"
-          }
-        });
-      }
-
-      media.onReady( start );
-
-    }
+    config: "config.json"
   }); //Butter
 }, false );
