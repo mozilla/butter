595370ada83f7b62ed95ace09e086e25d7a057d7
secretrobotron <secretrobotron@gmail.com>
2012-07-03 18:25:38 -0400

    [t1174] src/editor/module documentation

--- src/editor/module.js
+++ src/editor/module.js
@@ -2,77 +2,106 @@
  * If a copy of the MIT license was not distributed with this file, you can
  * obtain one at http://www.mozillapopcorn.org/butter-license.txt */
 
-(function() {
+/**
+ * Module: EditorModule
+ * 
+ * Butter Module for Editors
+ */
+define( [ "core/eventmanager",
+          "core/trackevent",
+          "./editor"
+        ], function(
+          EventManagerWrapper,
+          TrackEvent,
+          Editor
+        ){
 
-  define( [ "core/eventmanager",
-            "core/trackevent",
-            "./editor"
-          ], function(
-            EventManagerWrapper,
-            TrackEvent,
-            Editor
-          ){
+  /**
+   * Class: EventEditor
+   * 
+   * Module which provides Editor functionality to Butter
+   */
+  function EventEditor( butter, moduleOptions ){
 
-    function EventEditor( butter, moduleOptions ){
+    moduleOptions = moduleOptions || {};
 
-      moduleOptions = moduleOptions || {};
+    var _currentEditor,
+        _this = this;
 
-      var _currentEditor,
-          _this = this;
+    EventManagerWrapper( _this );
 
-      EventManagerWrapper( _this );
-
-      function openEditor( trackEvent ) {
-        var editorType = Editor.isRegistered( trackEvent.type ) || "default";
-        if( _currentEditor ) {
-          _currentEditor.close();
-        }
-        _currentEditor = Editor.create( editorType, butter );
-        _currentEditor.open( butter.ui.areas.editor.element, trackEvent );
-        return _currentEditor;
+    /**
+     * Member: openEditor
+     * 
+     * Open the editor corresponding to the type of the given TrackEvent
+     *
+     * @param {TrackEvent} trackEvent: TrackEvent to edit
+     */
+    function openEditor( trackEvent ) {
+      var editorType = Editor.isRegistered( trackEvent.type ) || "default";
+      if( _currentEditor ) {
+        _currentEditor.close();
       }
-
-      butter.listen( "trackeventcreated", function( e ){
-        if( [ "target", "media" ].indexOf( e.data.by ) > -1 && butter.ui.contentState === "timeline" ){
-          _this.edit( e.data.trackEvent );
-        }
-      });
-
-      this.edit = function( trackEvent ){
-        if ( !trackEvent || !( trackEvent instanceof TrackEvent ) ){
-          throw new Error( "trackEvent must be valid to start an editor." );
-        }
-        return openEditor( trackEvent );
-      };
-
-      butter.listen( "trackeventadded", function ( e ) {
-        var trackEvent = e.data;
-
-        var trackEventClicked = function ( e ) {
-          openEditor( trackEvent );
-        };
-
-        e.data.view.element.addEventListener( "mouseup", trackEventClicked, true );
-
-        butter.listen( "trackeventremoved", function ( e ) {
-          if ( e.data === trackEvent ) {
-            e.data.view.element.removeEventListener( "mouseup", trackEventClicked, true );
-          }
-        });
-
-      });
-
-      this._start = function( onModuleReady ){
-        onModuleReady();
-      };
-
+      _currentEditor = Editor.create( editorType, butter );
+      _currentEditor.open( butter.ui.areas.editor.element, trackEvent );
+      return _currentEditor;
     }
 
-    this.register = Editor.register;
+    // When a TrackEvent is somewhere in butter, open its editor immediately.
+    butter.listen( "trackeventcreated", function( e ){
+      if( [ "target", "media" ].indexOf( e.data.by ) > -1 && butter.ui.contentState === "timeline" ){
+        openEditor( e.data.trackEvent );
+      }
+    });
 
-    EventEditor.__moduleName = "editor";
+    /**
+     * Member: edit
+     * 
+     * Open the editor of corresponding to the type of the given TrackEvent
+     *
+     * @param {TrackEvent} trackEvent: TrackEvent to edit
+     */
+    this.edit = function( trackEvent ){
+      if ( !trackEvent || !( trackEvent instanceof TrackEvent ) ){
+        throw new Error( "trackEvent must be valid to start an editor." );
+      }
+      return openEditor( trackEvent );
+    };
 
-    return EventEditor;
+    butter.listen( "trackeventadded", function ( e ) {
+      var trackEvent = e.data;
 
-  }); //define
-}());
+      var trackEventClicked = function ( e ) {
+        openEditor( trackEvent );
+      };
+
+      e.data.view.element.addEventListener( "mouseup", trackEventClicked, true );
+
+      butter.listen( "trackeventremoved", function ( e ) {
+        if ( e.data === trackEvent ) {
+          e.data.view.element.removeEventListener( "mouseup", trackEventClicked, true );
+        }
+      });
+
+    });
+
+    /**
+     * Member: _start
+     * 
+     * Prepares this module for Butter startup
+     *
+     * @param {Function} onModuleReady: Callback to signify that module is ready
+     */
+    this._start = function( onModuleReady ){
+      onModuleReady();
+    };
+
+  }
+
+  this.register = Editor.register;
+
+  EventEditor.__moduleName = "editor";
+
+  return EventEditor;
+
+}); //define
