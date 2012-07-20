b67614d2d6d40c92ea89058e533da1232dabed21
secretrobotron <secretrobotron@gmail.com>
2012-07-20 12:17:34 -0400

    [style-linter] bit of cleanup and some docs

--- tools/mirror.js
+++ tools/mirror.js
@@ -1,5 +1,15 @@
 var fs = require('fs');
-
+/**
+ * Tokenizer
+ *
+ * Simple tokenizer. Chomps through data one segment at a time according to given rules.
+ *
+ * Usage:
+ * var t = new Tokenizer();                             // setup tokenizer
+ * t.addRule(/someRegex/, 'my-rule', optionalLength);   // add a rule
+ * t.on('my-rule', function(match){ ... });             // setup listeners for rule occurrences
+ * t.tokenize(inputData);                               // start tokenizing
+ */
 function Tokenizer(){
 
   var _rules = [];
@@ -8,25 +18,58 @@ function Tokenizer(){
   var _this = this;
   var _tokenListeners = {};
 
+  // For helping to make decisions about what to do when specific tokens are found. Useful in tandem with on().
   this.lastToken = null;
   this.lastCharacter = null;
 
+  /**
+   * Member: addRule
+   *
+   * Adds a rule to the tokenizer. Rules are regexps that are used to slowly chew through data.
+   * When the input string meets the requirements of a rule, the input string is shortened by the
+   * length of the string found, or by the given length (for more exact matching).
+   *
+   * @param {RegExp} rule: A regular expression used to determine the existence of a token.
+   * @param {String} type: Name of token.
+   * @param {Number} length: Optional. If length is given, it will be used to slice the input string if this rule is met.
+   */
   this.addRule = function(rule, type, length){
     _rules.push(rule);
     _types.push(type);
     _lengths.push(length);
   };
 
+  /**
+   * Member: on
+   *
+   * Listen for the occurrence of a token.
+   *
+   * @param {String} token: Name of token to listen for.
+   * @param {Function} listener: Callback to use when token is found.
+   */
   this.on = function(token, listener){
     _tokenListeners[token] = listener;
   };
 
-  this.tokenize = function(data, tokenFunction){
+  /**
+   * Member: tokenize
+   *
+   * Tokenize some data. This function will attempt to match each rule against the input string in the order they were added. When a rule
+   * is met, the input data will be shortened by the matched string's length (from the beginning). If a rule is not met, the string will
+   * be shortened by one character (from the beginning). This routine will continue until the input string is empty.
+   *
+   * @param {String} data: Data to tokenize.
+   */
+  this.tokenize = function(data){
     var successRule = -1;
     var rule;
     var match;
     var length;
+
+    // Spin until there is no data left.
     while(data.length){
+
+      // Compare each rule until one is found to match.
       for(var i=0, l=_rules.length; i<l; ++i){
         rule = _rules[i];
         match = data.match(rule);
@@ -34,17 +77,24 @@ function Tokenizer(){
           break;
         }
       }
+
+      // If a rule matched, tokenize!
       if(match){
+        // If there was a listener for this token, call it now.
         if(_tokenListeners[_types[i]]){
           _tokenListeners[_types[i]](match[0]);
         }
+        // Figure out the appropriate length to shorten the input string and shorten it.
         length = _lengths[i] || match[0].length;
         data = data.substr(length);
+
+        // Remember some important data for the next loop.
         _this.lastCharacter = match[0][match[0].length-1];
         _this.lastToken = _types[i];
         _this.lastMatch = match[0];
       }
       else{
+        // Remember some important data and march on.
         _this.lastCharacter = data[0];
         _this.lastMatch = null;
         _this.lastToken = null;
@@ -57,9 +107,11 @@ function Tokenizer(){
 function tokenize(fileName, data){
   var t = new Tokenizer();
 
+  // Used to determine on which line an error occured.
   var lineNumber = 1;
   var lines = data.split('\n');
 
+  // Lots of tokens commonly known for JavaScript.
   t.addRule(/^('[^']*'|"[^"]*")/, 'string');
   t.addRule(/\./, 'dot');
   t.addRule(/^\s+/, 'whitespace');
@@ -91,16 +143,20 @@ function tokenize(fileName, data){
   t.addRule(/^\n/, 'newline');
   t.addRule(/^[\w\d]+/, 'word');
 
+  // Output a well-formatted error.
   function reportError(error){
-    console.log(fileName + ':' + lineNumber + ':   ' + error + ' \n' + lines[lineNumber-1]);
+    console.log(fileName + ':' + lineNumber + ':  ' + error + ' \n' + lines[lineNumber-1]);
   }
 
+  // Figure out how many newlines were in a match and advance the counter.
   function collectNewlines(match){
     var newlineMatch = match.match(/\n/g);
     if(newlineMatch){
       lineNumber += newlineMatch.length;
     }
   }
+
+  // Listeners!
 
   t.on('function', function(){
     if(t.lastToken !== 'whitespace'){
@@ -124,12 +180,12 @@ function tokenize(fileName, data){
 
   t.on('left-curly-bracket', function(match){
     if(t.lastToken !== 'whitespace'){
-      reportError('no whitespace before "' + match + '" keyword. found "' + t.lastMatch + '" instead.' );
+      reportError('no whitespace before "' + match + '". found "' + t.lastMatch + '" instead.' );
     }
   });
 
   t.on('right-circle-bracket', function(match){
-    if(t.lastToken !== 'whitespace'){
+    if(['whitespace', 'left-circle-bracket'].indexOf(t.lastToken) === -1 ){
       reportError('no whitespace after "' + t.lastMatch + '". found "' + match + '" instead.' );
     }
   });
@@ -142,7 +198,7 @@ function tokenize(fileName, data){
 
   t.on('word', function(match){
     if(['whitespace', 'dot'].indexOf(t.lastToken) === -1){
-      reportError('!!!no whitespace before "' + match + '". found "' + t.lastMatch + '" instead.' );
+      reportError('no whitespace before "' + match + '". found "' + t.lastMatch + '" instead.' );
     }
   });
 
@@ -152,9 +208,16 @@ function tokenize(fileName, data){
     }
   });
 
+  t.on('assignment', function(match){
+    if(t.lastToken !== 'whitespace'){
+      reportError('no whitespace before "' + match + '". found "' + t.lastMatch + '" instead.' );
+    }
+  });
+
   t.tokenize(data);
 }
 
+// Process one file.
 function processFile(fileName, onFinished){
   fs.readFile(fileName, function(err, data){
     if(err){
@@ -162,16 +225,17 @@ function processFile(fileName, onFinished){
       process.exit(1);
     }
 
-    var splitData = data.toString().split("\n");
     tokenize(fileName, data.toString());
     onFinished();
   });
 }
 
+// Only continue if there are files given on the command line.
 if(process.argv.length > 2){
   var filesArray = process.argv.slice( 2 );
   var filesIndex = 0;
 
+  // After one file is finished, attempt to process the next.
   var onFinished = function(){
     filesIndex++;
     if(filesIndex < filesArray.length ){
@@ -183,5 +247,5 @@ if(process.argv.length > 2){
 }
 else{
   console.log("mirror.js - butter style linter");
-  console.log("usage: node mirror.js filename");
+  console.log("usage: node mirror.js filename [filename2 ...]");
 }
