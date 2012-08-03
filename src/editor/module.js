5083db6222fa0aea7a59db35e151d170152bc866
David Seifried <david.c.seifried@gmail.com>
2012-08-03 10:42:34 -0400

    [#1894] Editor will now open if the handle is clicked but not while resizing

--- src/editor/module.js
+++ src/editor/module.js
@@ -82,7 +82,7 @@ define( [ "core/eventmanager", "core/trackevent", "./editor",
 
       // Open a new editor on a single click
       var trackEventMouseUp = function ( e ) {
-        if( butter.selectedEvents.length === 1 && !trackEvent.dragging  && !e.target.classList.contains( "handle" ) ) {
+        if( butter.selectedEvents.length === 1 && !trackEvent.dragging && !view.resizing ) {
           _this.editTrackEvent( trackEvent );
         }
       };
