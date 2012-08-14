c54d0f456fac78864277a8089a2c900a9a2362c0
Matthew Schranz <schranz.m@gmail.com>
2012-08-14 11:58:40 -0400

    [#1801] Popcorn Maker titles plugin

--- templates/assets/plugins/titles/popcorn.titles.js
+++ templates/assets/plugins/titles/popcorn.titles.js
@@ -11,19 +11,32 @@
   
    **/
 
-  // Assumes the video is wrapped in a container.
-  function createContainer( context, id ) {
-    var ctxContainer = context.container = document.createElement( "div" ),
-        style = ctxContainer.style,
-        media = context.media;
+  function normalize( value, minWidth, maxWidth ) {
+    return Math.max( Math.min( value || 0, maxWidth ), minWidth );
+  }
 
-    ctxContainer.id = id || "popcorn-titles-" + Popcorn.guid();
-    style.position = "absolute";
-    style.left = 0;
-    style.bottom = "35px";
+  function validateHexColor( color ) {
+    return color.match ? color.match( /^#(?:[0-9a-fA-F]{3}){1,2}$/ ) : false;
+  }
 
-    media.parentNode.appendChild( ctxContainer );
-    return ctxContainer;
+  // From humph's text plugin
+  var escapeMap = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': '&quot;',
+    "'": '&#39;'
+  };
+
+  function escapeHTML( string ) {
+    return String( string ).replace( /&(?!\w+;)|[<>"']/g, function ( s ) {
+      return escapeMap[ s ] || s;
+    });
+  }
+
+  function newlineToBreak( string ) {
+    // Deal with both \r\n and \n
+    return string.replace( /\r?\n/gm, "<br>" );
   }
 
   Popcorn.plugin( "titles", {
@@ -32,109 +45,156 @@
       about: {
         name: "Popcorn titles Plugin",
         version: "0.1",
-        author: "@k88hudson"
+        author: "@k88hudson, @mjschranz"
       },
       options: {
-        start: {
-          elem: "input",
-          type: "text",
-          label: "In"
-        },
-        end: {
-          elem: "input",
-          type: "text",
-          label: "Out"
-        },
         text: {
           elem: "input",
           type: "text",
           label: "Text",
-          "default": "Popcorn.js"
+          "default": "Mozilla Popcorn"
         },
-        target: {
-          "default": "overlay"
-        },
-        innerContainerClasses: {
-          elem: "input",
-          type: "text",
-          label: "Inner Container Classes (separate with a space)",
-          "default": ""
-        },
-        containerClasses: {
-          elem: "input",
-          type: "text",
-          label: "Container Classes (separate with a space)",
-          "default": "popcorn-transition popcorn-titles"
-        },
-        activeClass: {
-          elem: "input",
-          type: "text",
-          label: "Active Class",
-          "default": "popcorn-on"
-        },
-        inactiveClass: {
-          elem: "input",
-          type: "text",
-          label: "Inactive Class",
-          "default": "popcorn-off"
-        },
-        transitionSpeed: {
+        position: {
           elem: "select",
-          options: ["slow", "normal", "fast"],
-          label: "Transition Duration",
-          "default": "normal"
+          options: [ "Center", "Bottom", "Left", "Right", "Top", "Custom" ],
+          values: [ "center", "bottom", "left", "right", "top", "custom"  ],
+          label: "Text Position",
+          "default": "center"
+        },
+        start: {
+          elem: "input",
+          type: "text",
+          label: "In",
+          group: "advanced"
+        },
+        end: {
+          elem: "input",
+          type: "text",
+          label: "Out",
+          group: "advanced"
+        },
+        transition: {
+          elem: "select",
+          options: [ "None", "Pop", "Fade", ],
+          values: [ "none", "pop", "popcorn-fade" ],
+          label: "Transition",
+          "default": "popcorn-fade"
+        },
+        fontFamily: {
+          elem: "select",
+          label: "Font",
+          styleClass: "",
+          googleFonts: true,
+          group: "advanced",
+          "default": "Merriweather"
+        },
+        fontSize: {
+          elem: "input",
+          type: "number",
+          label: "Font Size",
+          "default": 48,
+          units: "px",
+          group: "advanced"
+        },
+        fontColor: {
+          elem: "input",
+          type: "text",
+          label: "Font Colour",
+          "default": "#FFF",
+          group: "advanced"
+        },
+        fontWeight: {
+          elem: "input",
+          type: "checkbox",
+          label: "Bold",
+          "default": false,
+          group: "advanced"
+        },
+        fontItalics: {
+          elem: "input",
+          type: "checkbox",
+          label: "Italics",
+          "default": false,
+          group: "advanced"
+        },
+        textUnderline: {
+          elem: "input",
+          type: "checkbox",
+          label: "Underline",
+          "default": false,
+          group: "advanced"
+        },
+        left: {
+          elem: "input",
+          type: "number",
+          label: "Left",
+          units: "%",
+          hidden: true
+        },
+        top: {
+          elem: "input",
+          type: "number",
+          label: "Top",
+          units: "%",
+          hidden: true
         }
       }
     },
 
     _setup: function( options ) {
 
-      var target,
-          text = options.text,
-          innerContainer = document.createElement( "div" ),
-          innerContainerClasses = ( options.innerContainerClasses && options.innerContainerClasses.split( " " ) ) || [],
+      var target = Popcorn.dom.find( options.target ),
+          text = newlineToBreak( escapeHTML( options.text ) ),
           container = options._container = document.createElement( "div" ),
-          containerClasses = ( options.containerClasses && options.containerClasses.split( " " ) ) || [],
-          inactiveClass = options.inactiveClass,
-          // See DURATION section of popcorn.transition.css
-          transitionSpeed = options.transitionSpeed,
-          transitionClass = "popcorn-transition-" + transitionSpeed,
-          i, l;
+          innerContainer = document.createElement( "div" ),
+          fontSheet;
 
-      // --------------------------------------
-      // PREPARE THE TARGET
-
-      //Check if it exists
-      if ( options.target ) {
-        target = Popcorn.dom.find( options.target );
-      }
       if ( !target ) {
-        target = options._newContainer = createContainer( this, options.target );
+        target = this.media.parentNode;
       }
-      // Cache reference to actual target container
+
       options._target = target;
+      container.style.position = "absolute";
+      container.classList.add( "popcorn-titles" );
 
-
-      // --------------------------------------
-      // PREPARE THE CONTAINER
-
-      // Add each class to the inner container
-      for ( i = 0, l = innerContainerClasses.length; i < l; i++ ) {
-        innerContainer.classList.add( innerContainerClasses[ i ] );
+      if ( options.position === "custom" ) {
+        container.classList.add( "titles-custom" );
+        container.style.left = normalize( options.left || 15, 0, 97 ) + "%";
+        container.style.top = normalize( options.top || 15, 0, 97 ) + "%";
+      }
+      else {
+        container.classList.add( "titles-fixed" );
+        innerContainer.classList.add( options.position );
       }
 
-      // Add each class to the outer container
-      for ( i = 0, l = containerClasses.length; i < l; i++ ) {
-        container.classList.add( containerClasses[ i ] );
-      }
+      // Add transition class
+      container.classList.add( options.transition );
+      options._container.classList.add( "off" );
 
-      // Add transition styles and hide on setup
-      container.classList.add( transitionClass );
-      container.classList.add( inactiveClass );
-      //Add the text, and append to target.
-      innerContainer.innerHTML = text || "";
+      // Handle all custom fonts/styling
+      innerContainer.innerHTML = "<span>" + text + "</span>";
+      container.style.zIndex = +options.zindex;
       container.appendChild( innerContainer );
       target.appendChild( container );
+
+      container.style.fontStyle = options.fontItalics ? "italic" : "normal";
+      container.style.color = validateHexColor( options.fontColor ) ? options.fontColor : "#668B8B";
+      container.style.textDecoration = options.textUnderline ? "underline" : "none";
+      container.style.fontSize = options.fontSize ? normalize( options.fontSize, 8, 200 ) + "px" : "24px";
+      container.style.fontWeight = options.fontWeight ? "bold" : "normal";
+
+      fontSheet = document.createElement( "link" );
+      fontSheet.rel = "stylesheet";
+      fontSheet.type = "text/css";
+      options.fontFamily = options.fontFamily ? options.fontFamily.replace( /\s/g, "+" ) : "Tangerine";
+      // Store reference to generated sheet for removal later, remove any existing ones
+      options._fontSheet = fontSheet;
+      document.head.appendChild( fontSheet );
+
+      fontSheet.onload = function ( e ) {
+        container.style.fontFamily = options.fontFamily;
+      };
+      fontSheet.href = "http://fonts.googleapis.com/css?family=" + options.fontFamily;
 
       options.toString = function() {
         // use the default option if it doesn't exist
@@ -143,24 +203,22 @@
     },
 
     start: function( event, options ) {
-      options._container.classList.add( options.activeClass );
-      options._container.classList.remove( options.inactiveClass );
+      options._container.classList.remove( "off" );
+      options._container.classList.add( "on" );
     },
 
     end: function( event, options ) {
-      options._container.classList.add( options.inactiveClass );
-      options._container.classList.remove( options.activeClass );
+      options._container.classList.remove( "on" );
+      options._container.classList.add( "off" );
     },
 
     _teardown: function( options ) {
-      var target = options._target,
-          newContainer = options._newContainer;
-      if ( target ) {
-        target.removeChild( options._container );
+      if ( options._target ) {
+        options._target.removeChild( options._container );
       }
-      //If we created a new container, we should remove it now.
-      if ( newContainer && newContainer.parentNode ) {
-        newContainer.parentNode.removeChild( newContainer );
+
+      if ( options._fontSheet ) {
+        document.head.removeChild( options._fontSheet );
       }
     }
   });
