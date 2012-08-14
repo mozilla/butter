21da61657dad419f0a44db7e3a4ce60e69f1eb85
David Seifried <david.c.seifried@gmail.com>
2012-08-14 16:41:54 -0400

    [#1975] Added tooltips to projectName element and did a bit of error checking when hitting save

--- src/ui/header.js
+++ src/ui/header.js
@@ -1,5 +1,5 @@
-define( [ "dialog/dialog", "util/lang", "text!layouts/header.html" ],
-  function( Dialog, Lang, HEADER_TEMPLATE ){
+define( [ "dialog/dialog", "util/lang", "text!layouts/header.html", "ui/widget/tooltip" ],
+  function( Dialog, Lang, HEADER_TEMPLATE, ToolTip ){
 
   var DEFAULT_AUTH_BUTTON_TEXT = "<span class='icon-user'></span> Sign In / Sign Up",
       DEFAULT_AUTH_BUTTON_TITLE = "Sign in or sign up with Persona";
@@ -13,10 +13,21 @@ define( [ "dialog/dialog", "util/lang", "text!layouts/header.html" ],
         _saveButton = _rootElement.querySelector( ".butter-save-btn" ),
         _buttonGroup = _rootElement.querySelector( ".butter-login-project-info"),
         _authButton = _rootElement.querySelector( ".butter-login-btn" ),
+        _projectTitle = _rootElement.querySelector( ".butter-project-title" ),
+        _projectName = _projectTitle.querySelector( ".butter-project-name" ),
         _loginClass = "butter-login-true",
-        _activeClass = "btn-green";
+        _activeClass = "btn-green",
+        _noProjectNameToolTip,
+        _projectTitlePlaceHolderText = _projectName.innerHTML;
+
+    // create a tooltip for the projectName element
+    ToolTip.create({
+      element: _projectTitle,
+      top: "43px"
+    });
 
     _this.element = _rootElement;
+    ToolTip.apply( _projectTitle );
 
     function authenticationRequired( successCallback, errorCallback ){
       if ( butter.cornfield.authenticated() && successCallback && typeof successCallback === "function" ) {
@@ -115,9 +126,82 @@ define( [ "dialog/dialog", "util/lang", "text!layouts/header.html" ],
       doSave( publish );
     }
 
+    function onMouseOver() {
+      _projectTitle.removeEventListener( "mouseover", onMouseOver, false );
+      _projectTitle.removeChild( _noProjectNameToolTip );
+    }
+
+    function createErrorToolTip() {
+      _projectTitle.removeEventListener( "mouseover", onMouseOver, false );
+
+      if ( _projectTitle.querySelector( ".tooltip-error" ) ) {
+        _projectTitle.removeChild( _noProjectNameToolTip );
+      }
+
+      if ( butter.project.name && butter.project.name !== _projectTitlePlaceHolderText ) {
+        return;
+      }
+
+      _noProjectNameToolTip = ToolTip.create({
+        message: "Please give your project a name before saving",
+        hidden: false,
+        element: _projectTitle,
+        top: "43px"
+      });
+
+      _noProjectNameToolTip.classList.add( "tooltip-error" );
+      _projectTitle.addEventListener( "mouseover", onMouseOver, false );
+      return true;
+    }
+
+    function onKeyPress( e ) {
+      var node = _projectTitle.querySelector( ".butter-project-name" );
+
+      // if this wasn't the 'enter' key, return early
+      if ( e.keyCode !== 13 ) {
+        return;
+      }
+
+      node.blur();
+      node.removeEventListener( "keypress", onKeyPress, false );
+    }
+
+    function onBlur() {
+      var node = _projectTitle.querySelector( ".butter-project-name" );
+
+      _projectName.innerHTML = node.value || _projectTitlePlaceHolderText;
+      butter.project.name = node.value;
+
+      createErrorToolTip();
+      _projectTitle.replaceChild( _projectName, node );
+      node.removeEventListener( "blur", onBlur, false );
+      _projectTitle.addEventListener( "click", projectNameClick, false );
+    }
+
     _saveButton.addEventListener( "click", function( e ){
+      if ( createErrorToolTip() ) {
+        return;
+      }
       authenticationRequired( prepare );
     }, false );
+
+    function projectNameClick( e ) {
+      var input = document.createElement( "input" );
+
+      input.type = "text";
+
+      input.placeholder = _projectTitlePlaceHolderText;
+      input.classList.add( "butter-project-name" );
+      input.value = _projectName.innerHTML !== _projectTitlePlaceHolderText ? _projectName.innerHTML : "";
+      _projectTitle.replaceChild( input, _projectName );
+      _projectTitle.removeEventListener( "click", projectNameClick, false );
+      input.focus();
+      input.addEventListener( "blur", onBlur, false );
+      input.addEventListener( "keypress", onKeyPress, false );
+    }
+
+    _projectTitle.addEventListener( "click", projectNameClick, false );
+
 
     function doLogout() {
       butter.cornfield.logout( logoutDisplay );
