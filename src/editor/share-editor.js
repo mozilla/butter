64692163ed1a244828fe8d399cf0f4da5494f0da
David Seifried <david.c.seifried@gmail.com>
2012-08-14 15:26:05 -0400

    [#1933] Share Editor

--- src/editor/share-editor.js
+++ src/editor/share-editor.js
@@ -2,20 +2,186 @@
  * If a copy of the MIT license was not distributed with this file, you can
  * obtain one at http://www.mozillapopcorn.org/butter-license.txt */
 
-define( [ "editor/editor", "editor/base-editor", "text!layouts/share-editor.html" ],
-  function( Editor, BaseEditor, LAYOUT_SRC ) {
+define([ "editor/editor", "editor/base-editor", "ui/user-data",
+          "text!layouts/share-editor.html", "util/social-media" ],
+  function( Editor, BaseEditor, UserData, LAYOUT_SRC, SocialMedia ) {
 
   Editor.register( "share-properties", LAYOUT_SRC, function( rootElement, butter, compiledLayout ) {
+    var socialMedia = SocialMedia(),
+        editorContainer = rootElement.querySelector( ".editor-container" ),
+        saveContainer = rootElement.querySelector( ".save-container" ),
+        projectURL = editorContainer.querySelector( ".butter-project-url" ),
+        projectEmbedURL = editorContainer.querySelector( ".butter-project-embed-url" ),
+        embedSize = editorContainer.querySelector( ".butter-embed-size" ),
+        shareFacebook = editorContainer.querySelector( ".butter-share-facebook" ),
+        shareTwitter = editorContainer.querySelector( ".butter-share-twitter" ),
+        shareGoogle = editorContainer.querySelector( ".butter-share-google" ),
+        projectNameWrapper = saveContainer.querySelector( ".butter-project-name-wrapper" ),
+        projectName = projectNameWrapper.querySelector( ".butter-project-name" ),
+        loginBtn = saveContainer.querySelector( ".butter-login-btn" ),
+        saveBtn = saveContainer.querySelector( ".butter-save-btn" ),
+        userData = new UserData( butter ),
+        embedDimensions = embedSize.value.split( "x" ),
+        embedWidth = embedDimensions[ 0 ],
+        embedHeight = embedDimensions[ 1 ],
+        tooltip;
+
+    function onMouseOver() {
+      projectNameWrapper.removeEventListener( "mouseover", onMouseOver, false );
+      if ( tooltip ) {
+        projectNameWrapper.removeChild( tooltip );
+      }
+    }
+
+    function fadeEditorContainer() {
+      editorContainer.classList.add( "fade-container" );
+      saveContainer.classList.remove( "hide-container" );
+    }
+
+    function displayLogin() {
+      resetInput();
+      embedSize.disabled = true;
+      saveBtn.removeEventListener( "click", save, false );
+      saveBtn.classList.add( "hide-container" );
+      saveContainer.classList.remove( "butter-login-true" );
+      loginBtn.classList.remove( "hide-container" );
+      loginBtn.addEventListener( "click", login, false );
+    }
+
+    function resetInput() {
+      shareFacebook.innerHTML = "";
+      shareTwitter.innerHTML = "";
+      shareGoogle.innerHTML = "";
+      projectURL.value = "";
+      projectEmbedURL.value = "";
+      fadeEditorContainer();
+    }
+
+    function projectSaved( e ) {
+      if ( e.data ) {
+        return;
+      }
+      userData.save(function() {
+        butter.unlisten( "projectsaved", projectSaved );
+        displayEditor();
+      });
+    }
+
+    function displaySave() {
+      resetInput();
+      embedSize.disabled = true;
+      butter.listen( "projectsaved", projectSaved );
+      loginBtn.classList.add( "hide-container" );
+      loginBtn.removeEventListener( "click", login, false );
+      saveBtn.classList.remove( "hide-container" );
+      saveContainer.classList.add( "butter-login-true" );
+      saveBtn.addEventListener( "click", save, false );
+    }
+
+    function updateEmbed( url ) {
+      projectEmbedURL.value = "<iframe src='" + url + "' width='" + embedWidth + "' height='" + embedHeight + "'" +
+      " frameborder='0' mozallowfullscreen webkitallowfullscreen allowfullscreen></iframe>";
+    }
+
+    function displayEditor() {
+
+      if ( !butter.cornfield.authenticated() ) {
+        displayLogin();
+        return;
+      }
+
+      embedSize.disabled = false;
+      saveContainer.classList.add( "hide-container" );
+      editorContainer.classList.remove( "fade-container" );
+      projectName.value = "";
+
+      butter.cornfield.publish( butter.project.id, function( e ) {
+        if ( e.error !== "okay" ) {
+          userData.showErrorDialog( "There was a problem saving your project. Please try again." );
+          return;
+        }
+        projectURL.value = e.url;
+        updateEmbed( projectURL.value.replace( "/v/", "/e/" ) );
+        socialMedia.hotLoad( shareFacebook, socialMedia.facebook, e.url );
+        socialMedia.hotLoad( shareTwitter, socialMedia.twitter, e.url );
+        socialMedia.hotLoad( shareGoogle, socialMedia.google, e.url );
+      });
+    }
+
+    function login() {
+      if ( !butter.project.name ) {
+        butter.project.name = projectName.value;
+      } else {
+        projectName.value = butter.project.name;
+      }
+      var doNotUpdate = true;
+
+      userData.authenticationRequired(function() {
+        if ( !butter.project.name ) {
+          displaySave();
+          doNotUpdate = false;
+        } else {
+          save();
+        }
+        butter.dispatch( "authenticated", doNotUpdate );
+      });
+    }
+
+    function save() {
+      if ( !butter.project.name ) {
+        butter.project.name = projectName.value;
+      } else {
+        projectName.value = butter.project.name;
+      }
+
+      if ( !butter.project.name ) {
+        tooltip = userData.createErrorToolTip( projectNameWrapper, {
+          message: "Please give your project a name before saving",
+          hidden: false,
+          element: projectNameWrapper,
+          top: "33px"
+        }, onMouseOver );
+        return;
+      }
+
+      butter.dispatch( "projectsaved", true );
+      userData.save(function() {
+        displayEditor();
+      }, null );
+    }
+
+    embedSize.addEventListener( "change", function() {
+      embedDimensions = embedSize.value.split( "x" );
+      embedWidth = embedDimensions[ 0 ];
+      embedHeight = embedDimensions[ 1 ];
+      updateEmbed( projectURL.value.replace( "/v/", "/e/" ) );
+    }, false);
+
+    butter.listen( "authenticated", function( e ) {
+      if ( e.data ) {
+        return;
+      }
+      if ( !butter.project.name ) {
+        displaySave();
+      } else {
+        save();
+      }
+    });
+    butter.listen( "logout", displayLogin );
+    butter.listen( "projectupdated", login );
 
     Editor.BaseEditor( this, butter, rootElement, {
       open: function() {
-    
+        if ( !butter.cornfield.authenticated() ) {
+          displayLogin();
+        } else if ( !butter.project.name ) {
+          displaySave();
+        } else {
+          displayEditor();
+        }
       },
       close: function() {
-
       }
     });
-
   });
-
 });
---
+++ src/ui/user-data.js
@@ -0,0 +1,173 @@
+/* This Source Code Form is subject to the terms of the MIT license
+ * If a copy of the MIT license was not distributed with this file, you can
+ * obtain one at http://www.mozillapopcorn.org/butter-license.txt */
+
+/*
+ * This file exposes various helper methods centered around user-data, such as logging in,
+ * saving, and logging out.
+ */
+define( [ "dialog/dialog", "util/lang", "text!layouts/header.html", "ui/widget/tooltip" ],
+  function( Dialog, Lang, HEADER_TEMPLATE, ToolTip ) {
+
+  return function( butter ) {
+    var _this = this,
+        _rootElement = Lang.domFragment( HEADER_TEMPLATE, ".butter-header" ),
+        _saveButton = _rootElement.querySelector( ".butter-save-btn" ),
+        _buttonGroup = _rootElement.querySelector( ".butter-login-project-info"),
+        _authButton = _rootElement.querySelector( ".butter-login-btn" ),
+        _projectTitle = _rootElement.querySelector( ".butter-project-title" ),
+        _projectName = _projectTitle.querySelector( ".butter-project-name" );
+
+    this.element = _rootElement;
+
+    /*
+     * Method: showErrorDialog
+     *
+     * Opens a dialog that informs the user an error has occured
+     * @params {String} message: the message to be displayed to the user
+     * @params {Function} callback: option callback function to be called once the dialog has be closed
+     */
+    this.showErrorDialog = function( message, callback ) {
+      var dialog = Dialog.spawn( "error-message", {
+        data: message,
+        events: {
+          cancel: function( e ) {
+            dialog.close();
+            if ( callback ) {
+              callback();
+            }
+          }
+        }
+      });
+      dialog.open();
+    };
+
+    this.authenticationRequired = function( successCallback, errorCallback ) {
+      if ( butter.cornfield.authenticated() && successCallback && typeof successCallback === "function" ) {
+        successCallback();
+        return;
+      }
+
+      butter.cornfield.login(function( response ) {
+        if ( !response.error ) {
+          butter.cornfield.list(function( listResponse ) {
+            if ( successCallback ) {
+              successCallback();
+            }
+          });
+        } else {
+          _this.showErrorDialog( "There was an error logging in. Please try again." );
+          if ( errorCallback ) {
+            errorCallback();
+          }
+        }
+      });
+    };
+
+
+    /*
+     * Method: save
+     *
+     * Allows the user the save their project.
+     * @param {function} successCallback: A callback function that is fired when the user has successfully saved
+     * @param {function} errorCallback: A callback function that is fired when the user attempts to save with no project name
+     */
+    this.save = function( successCallback, errorCallback ) {
+
+      function execute(){
+        var saveString;
+
+        butter.project.data = butter.exportProject();
+        saveString = JSON.stringify( butter.project, null, 4 );
+        butter.ui.loadIndicator.start();
+        butter.cornfield.save( butter.project.id, saveString, function( e ) {
+          butter.ui.loadIndicator.stop();
+          if ( e.error !== "okay" || !e.project || !e.project._id ) {
+            _this.showErrorDialog( "There was a problem saving your project. Please try again." );
+            return;
+          }
+          butter.project.id = e.project._id;
+          if ( successCallback ) {
+            successCallback();
+          }
+        });
+      }
+
+      if ( !butter.project.name && errorCallback ) {
+        errorCallback();
+      } else {
+        execute();
+      }
+    };
+
+    /*
+     * Method: createErrorToolTip
+     *
+     * Creates an error ToolTip for the specified element
+     *
+     * @param {DOMElement} element: the element in which to display the tooltip over
+     * @param {Object} options: An options object for the ToolTip that is to be created
+     * @param {Function} mouseOverListener: A callback function that is called when moused over the element passed in
+     * returns the ToolTip that was created
+     */
+    this.createErrorToolTip = function( element, options, mouseOverListener ) {
+      var tooltip;
+
+      element.removeEventListener( "mouseover", mouseOverListener, false );
+
+      if ( element.querySelector( ".tooltip-error" ) ) {
+        element.removeChild( element.querySelector( ".tooltip-error" ) );
+      }
+
+      tooltip = ToolTip.create( options );
+
+      tooltip.classList.add( "tooltip-error" );
+      element.addEventListener( "mouseover", mouseOverListener, false );
+      return tooltip;
+    };
+
+    this.logout = function( callback ) {
+      butter.dispatch( "logout" );
+      butter.cornfield.logout( callback );
+    };
+
+    Object.defineProperties( this, {
+      rootElement: {
+        enumerable: true,
+        get: function() {
+          return _rootElement;
+        }
+      },
+      saveButton: {
+        enumerable: true,
+        get: function() {
+          return _saveButton;
+        }
+      },
+      buttonGroup: {
+        enumerable: true,
+        get: function() {
+          return _buttonGroup;
+        }
+      },
+      authButton: {
+        enumerable: true,
+        get: function() {
+          return _authButton;
+        }
+      },
+      projectTitle: {
+        enumerable: true,
+        get: function() {
+          return _projectTitle;
+        }
+      },
+      projectName: {
+        enumerable: true,
+        get: function() {
+          return _projectName;
+        }
+      }
+    });
+  };
+});
