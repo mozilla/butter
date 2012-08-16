61af9e3150609607e8e1f2b96b8167f34b60bc87
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2012-08-16 11:11:52 -0400

    [#1860] Replace current export page with embed

--- cornfield/app.js
+++ cornfield/app.js
@@ -14,7 +14,11 @@ var express = require('express'),
     CONFIG = require('config'),
     TEMPLATES_DIR =  CONFIG.dirs.templates,
     PUBLISH_DIR = CONFIG.dirs.publish,
+    PUBLISH_DIR_V = path.join( PUBLISH_DIR, 'v' ),
+    PUBLISH_DIR_E = path.join( PUBLISH_DIR, 'e' ),
     PUBLISH_PREFIX = CONFIG.dirs.hostname,
+    PUBLISH_PREFIX_V = CONFIG.dirs.hostname + "/v",
+    PUBLISH_PREFIX_E = CONFIG.dirs.hostname + "/e",
     WWW_ROOT = path.resolve( CONFIG.dirs.wwwRoot || path.join( __dirname, ".." ) ),
     VALID_TEMPLATES = CONFIG.templates,
     EXPORT_ASSETS = CONFIG.exportAssets;
@@ -69,6 +73,12 @@ CONFIG.session.store = new MongoStore({ db: "test" });
 if ( !path.existsSync( PUBLISH_DIR ) ) {
   fs.mkdirSync( PUBLISH_DIR );
 }
+if ( !path.existsSync( PUBLISH_DIR_V ) ) {
+  fs.mkdirSync( PUBLISH_DIR_V );
+}
+if ( !path.existsSync( PUBLISH_DIR_E ) ) {
+  fs.mkdirSync( PUBLISH_DIR_E );
+}
 
 app.configure( function() {
   app.use( express.logger( CONFIG.logger ) )
@@ -99,18 +109,41 @@ app.configure( 'development', function() {
 
 require('express-browserid').plugAll(app);
 
-function writeEmbed( projectEmbedPath, embedUrl, res, url, data ) {
+function writeEmbedShell( path, res, url, data, callback ) {
+  if( !writeEmbedShell.templateFn ) {
+    writeEmbedShell.templateFn = jade.compile( fs.readFileSync( 'views/embed-shell.jade', 'utf8' ),
+                                          { filename: 'embed-shell.jade', pretty: true } );
+  }
+
+  fs.writeFile( path, writeEmbedShell.templateFn( data ), function( err ){
+    if( err ){
+      res.json({ error: 'internal file error' }, 500);
+      return;
+    }
+    if( callback ) {
+      callback();
+    } else {
+      res.json({ error: 'okay', url: url });
+    }
+  });
+}
+
+function writeEmbed( path, res, url, data, callback ) {
   if( !writeEmbed.templateFn ) {
     writeEmbed.templateFn = jade.compile( fs.readFileSync( 'views/embed.jade', 'utf8' ),
                                           { filename: 'embed.jade', pretty: true } );
   }
 
-  fs.writeFile( projectEmbedPath, writeEmbed.templateFn( data ), function( err ){
+  fs.writeFile( path, writeEmbed.templateFn( data ), function( err ){
     if( err ){
       res.json({ error: 'internal file error' }, 500);
       return;
     }
-    res.json({ error: 'okay', url: url });
+    if( callback ) {
+      callback();
+    } else {
+      res.json({ error: 'okay', url: url });
+    }
   });
 }
 
@@ -138,155 +171,157 @@ app.post( '/api/publish/:id', filter.isLoggedIn, filter.isStorageAvailable, func
       }
     }
 
-    if ( project ) {
-      var template = project.template,
-          customData = JSON.parse( project.customData, sanitizer.escapeHTMLinJSON );
-
-      if ( template && VALID_TEMPLATES[ template ] ) {
-        var projectPath = PUBLISH_DIR + "/" + id + ".html",
-            projectEmbedPath = PUBLISH_DIR + "/" + id + "-embed.html",
-            url = PUBLISH_PREFIX + "/" + id + ".html",
-            embedUrl = PUBLISH_PREFIX + "/" + id + "-embed.html",
-            projectData = JSON.parse( project.data, sanitizer.escapeHTMLinJSON ),
-            templateConfig = templateConfigs[ template ],
-            templateFile = templateConfig.template,
-            baseHref;
-
-        fs.readFile( templateFile, 'utf8', function( err, data ){
-
-          if ( err ) {
-            res.json( { error: 'error reading template file' }, 500 );
-            return;
-          }
-
-          var headEndTagIndex,
-              bodyEndTagIndex,
-              externalAssetsString = '',
-              popcornString = '',
-              customDataString = '',
-              currentMedia,
-              currentTrack,
-              currentTrackEvent,
-              mediaPopcornOptions,
-              templateURL,
-              baseString,
-              headStartTagIndex,
-              templateScripts,
-              startString,
-              numSources,
-              j, k, len;
-
-          templateURL = templateFile.substring( templateFile.indexOf( '/templates' ), templateFile.lastIndexOf( '/' ) );
-          baseHref = PUBLISH_PREFIX + templateURL + "/";
-          baseString = '\n  <base href="' + baseHref + '"/>';
-
-          // look for script tags with data-butter-exclude in particular (e.g. butter's js script)
-          data = data.replace( /\s*<script[\.\/='":_\-\w\s]*data-butter-exclude[\.\/='":_\-\w\s]*><\/script>/g, '' );
-
-          // Adding  to cut out the actual head tag
-          headStartTagIndex = data.indexOf( '<head>' ) + 6;
-          headEndTagIndex = data.indexOf( '</head>' );
-          bodyEndTagIndex = data.indexOf( '</body>' );
-
-          templateScripts = data.substring( headStartTagIndex, headEndTagIndex );
-          startString = data.substring( 0, headStartTagIndex );
-
-          externalAssetsString += '\n';
-          for ( i = 0; i < EXPORT_ASSETS.length; ++i ) {
-            externalAssetsString += '  <script src="' + path.relative( templateFile, path.resolve( EXPORT_ASSETS[ i ] ) ) + '"></script>\n';
-          }
-
-          // If the template has custom plugins defined in it's config, add them to our exported page
-          if ( templateConfig.plugin && templateConfig.plugin.plugins ) {
-            var plugins = templateConfig.plugin.plugins;
-
-            for ( i = 0, len = plugins.length; i < len; i++ ) {
-              externalAssetsString += '\n  <script src="' + PUBLISH_PREFIX + '/' + plugins[ i ].path.split( '{{baseDir}}' ).pop() + '"></script>';
-            }
-            externalAssetsString += '\n';
-          }
-
-          popcornString += '<script>';
-
-          for ( i = 0; i < projectData.media.length; ++i ) {
-            var mediaUrls,
-                mediaUrlsString = '[ "';
-
-            currentMedia = projectData.media[ i ];
-            // We expect a string (one url) or an array of url strings.
-            // Turn a single url into an array of 1 string.
-            mediaUrls = typeof currentMedia.url === "string" ? [ currentMedia.url ] : currentMedia.url;
-            mediaPopcornOptions = currentMedia.popcornOptions || {};
-            // Force the Popcorn instance we generate to have an ID we can query.
-            mediaPopcornOptions.id = "Butter-Generated";
-
-            numSources = mediaUrls.length;
-
-            for ( k = 0; k < numSources - 1; k++ ) {
-              mediaUrlsString += mediaUrls[ k ] + '" , "';
-            }
-            mediaUrlsString += mediaUrls[ numSources - 1 ] + '" ]';
-
-            popcornString += '\n(function(){';
-            popcornString += '\nvar popcorn = Popcorn.smart("#' + currentMedia.target + '", ' +
-                             mediaUrlsString + ', ' + JSON.stringify( mediaPopcornOptions ) + ');';
-            for ( j = 0; j < currentMedia.tracks.length; ++ j ) {
-              currentTrack = currentMedia.tracks[ j ];
-              for ( k = 0; k < currentTrack.trackEvents.length; ++k ) {
-                currentTrackEvent = currentTrack.trackEvents[ k ];
-                popcornString += '\npopcorn.' + currentTrackEvent.type + '(';
-                popcornString += JSON.stringify( currentTrackEvent.popcornOptions, null, 2 );
-                popcornString += ');';
-              }
-            }
-
-            if ( currentMedia.controls ) {
-              popcornString += "\npopcorn.controls( true );\n";
-            } else {
-              popcornString += "\npopcorn.controls( false );\n";
-            }
-            popcornString += '}());\n';
-          }
-          popcornString += '</script>\n';
-
-          customDataString = '\n<script type="application/butter-custom-data">\n' + JSON.stringify( customData, null, 2 ) + '\n</script>\n';
-          data = startString + baseString + templateScripts + externalAssetsString + data.substring( headEndTagIndex, bodyEndTagIndex ) + customDataString + popcornString + data.substring( bodyEndTagIndex );
-
-          fs.writeFile( projectPath, data, function( err ){
-            if( err ){
-              res.json({ error: 'internal file error' }, 500);
-              return;
-            }
-
-            // Write out embed HTML, too.
-            writeEmbed( projectEmbedPath, embedUrl, res, url, {
-              id: id,
-              author: email,
-              title: "todo",
-              baseHref: baseHref,
-              templateScripts: templateScripts,
-              externalAssets: externalAssetsString,
-              customData: customDataString,
-              // XXX: need a better way to wrap function, DOM needs to be ready
-              popcorn: popcornString.replace( /^\(function\(\)\{/m, "Popcorn( function(){" )
-                                    .replace( /\}\(\)\);$/m, "});" )
-              // XXX: need a better way to force controls off in embed.
-                                    .replace( "popcorn.controls( true );", "popcorn.controls( false );" )
-              // XXX: need a better way to force the use of the #smart div
-                                    .replace( /Popcorn.smart\("#([^"]+)"/, "Popcorn.smart(\"#embed-smart\"" )
-            });
-          });
-        });
-      }
-      else {
-        res.json({ error: 'template not found' }, 500);
-        return;
-      }
-    }
-    else {
+    if ( !project ) {
       res.json({ error: 'project not found' }, 500);
       return;
     }
+
+    var template = project.template,
+        customData = JSON.parse( project.customData, sanitizer.escapeHTMLinJSON );
+
+    if( !( template && VALID_TEMPLATES[ template ] ) ) {
+      res.json({ error: 'template not found' }, 500);
+      return;
+    }
+
+    var projectData = JSON.parse( project.data, sanitizer.escapeHTMLinJSON ),
+        templateConfig = templateConfigs[ template ],
+        templateFile = templateConfig.template,
+        baseHref;
+
+    fs.readFile( templateFile, 'utf8', function( err, data ){
+      if ( err ) {
+        res.json( { error: 'error reading template file' }, 500 );
+        return;
+      }
+
+      var headEndTagIndex,
+          bodyEndTagIndex,
+          externalAssetsString = '',
+          popcornString = '',
+          customDataString = '',
+          currentMedia,
+          currentTrack,
+          currentTrackEvent,
+          mediaPopcornOptions,
+          templateURL,
+          baseString,
+          headStartTagIndex,
+          templateScripts,
+          startString,
+          numSources,
+          j, k, len;
+
+      templateURL = templateFile.substring( templateFile.indexOf( '/templates' ), templateFile.lastIndexOf( '/' ) );
+      baseHref = PUBLISH_PREFIX + templateURL + "/";
+      baseString = '\n  <base href="' + baseHref + '"/>';
+
+      // look for script tags with data-butter-exclude in particular (e.g. butter's js script)
+      data = data.replace( /\s*<script[\.\/='":_\-\w\s]*data-butter-exclude[\.\/='":_\-\w\s]*><\/script>/g, '' );
+
+      // Adding  to cut out the actual head tag
+      headStartTagIndex = data.indexOf( '<head>' ) + 6;
+      headEndTagIndex = data.indexOf( '</head>' );
+      bodyEndTagIndex = data.indexOf( '</body>' );
+
+      templateScripts = data.substring( headStartTagIndex, headEndTagIndex );
+      startString = data.substring( 0, headStartTagIndex );
+
+      externalAssetsString += '\n';
+      for ( i = 0; i < EXPORT_ASSETS.length; ++i ) {
+        externalAssetsString += '  <script src="' + path.relative( templateFile, path.resolve( EXPORT_ASSETS[ i ] ) ) + '"></script>\n';
+      }
+
+      // If the template has custom plugins defined in it's config, add them to our exported page
+      if ( templateConfig.plugin && templateConfig.plugin.plugins ) {
+        var plugins = templateConfig.plugin.plugins;
+        for ( i = 0, len = plugins.length; i < len; i++ ) {
+          externalAssetsString += '\n  <script src="' + PUBLISH_PREFIX + '/' + plugins[ i ].path.split( '{{baseDir}}' ).pop() + '"></script>';
+        }
+        externalAssetsString += '\n';
+      }
+
+      popcornString += '<script>';
+
+      for ( i = 0; i < projectData.media.length; ++i ) {
+        var mediaUrls,
+            mediaUrlsString = '[ "';
+
+        currentMedia = projectData.media[ i ];
+        // We expect a string (one url) or an array of url strings.
+        // Turn a single url into an array of 1 string.
+        mediaUrls = typeof currentMedia.url === "string" ? [ currentMedia.url ] : currentMedia.url;
+        mediaPopcornOptions = currentMedia.popcornOptions || {};
+        // Force the Popcorn instance we generate to have an ID we can query.
+        mediaPopcornOptions.id = "Butter-Generated";
+
+        numSources = mediaUrls.length;
+
+        for ( k = 0; k < numSources - 1; k++ ) {
+          mediaUrlsString += mediaUrls[ k ] + '" , "';
+        }
+        mediaUrlsString += mediaUrls[ numSources - 1 ] + '" ]';
+
+        popcornString += '\n(function(){';
+        popcornString += '\nvar popcorn = Popcorn.smart("#' + currentMedia.target + '", ' +
+                         mediaUrlsString + ', ' + JSON.stringify( mediaPopcornOptions ) + ');';
+        for ( j = 0; j < currentMedia.tracks.length; ++ j ) {
+          currentTrack = currentMedia.tracks[ j ];
+          for ( k = 0; k < currentTrack.trackEvents.length; ++k ) {
+            currentTrackEvent = currentTrack.trackEvents[ k ];
+            popcornString += '\npopcorn.' + currentTrackEvent.type + '(';
+            popcornString += JSON.stringify( currentTrackEvent.popcornOptions, null, 2 );
+            popcornString += ');';
+          }
+        }
+
+        if ( currentMedia.controls ) {
+          popcornString += "\npopcorn.controls( true );\n";
+        } else {
+          popcornString += "\npopcorn.controls( false );\n";
+        }
+        popcornString += '}());\n';
+      }
+      popcornString += '</script>\n';
+
+      customDataString = '\n<script type="application/butter-custom-data">\n' +
+                         JSON.stringify( customData, null, 2 ) + '\n</script>\n';
+      data = startString + baseString + templateScripts + externalAssetsString +
+             data.substring( headEndTagIndex, bodyEndTagIndex ) + customDataString +
+             popcornString + data.substring( bodyEndTagIndex );
+
+      function publishEmbedShell() {
+        // Write out embed shell HTML
+        writeEmbedShell( path.join( PUBLISH_DIR_V, id + ".html" ),
+                         res, PUBLISH_PREFIX_V + "/" + id + ".html",
+                         {
+                           author: email,
+                           projectName: project.name,
+                           embedSrc: PUBLISH_PREFIX_E + "/" + id + ".html"
+                         });
+      }
+
+      writeEmbed( path.join( PUBLISH_DIR_E, id + ".html" ),
+                  res, path.join( PUBLISH_PREFIX_E, id + ".html" ),
+                  {
+                    id: id,
+                    author: email,
+                    title: project.name,
+                    baseHref: baseHref,
+                    templateScripts: templateScripts,
+                    externalAssets: externalAssetsString,
+                    customData: customDataString,
+                    // XXX: need a better way to wrap function, DOM needs to be ready
+                    popcorn: popcornString.replace( /^\(function\(\)\{/m, "Popcorn( function(){" )
+                                          .replace( /\}\(\)\);$/m, "});" )
+                    // XXX: need a better way to force controls off in embed.
+                                          .replace( "popcorn.controls( true );", "popcorn.controls( false );" )
+                    // XXX: need a better way to force the use of the #smart div
+                                          .replace( /Popcorn.smart\("#([^"]+)"/, "Popcorn.smart(\"#embed-smart\"" )
+                  },
+                  publishEmbedShell );
+
+    });
   });
 });
 
