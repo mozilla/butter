65e97ba4d38e600b798c7351bc3b30f477db55b2
Matthew Schranz <schranz.m@gmail.com>
2012-08-26 19:13:26 -0400

    [#1869] Remove old testing template

--- templates/test-init.js
+++
@@ -1,5 +0,0 @@
-/* Note: This code is just for testing script functionality on Butter init.
- * Disregard unless you want advanced functionality. */
-(function(){
-  console.log( "inline test init" );
-}());
