6e7a66b5025ad455a3fa23a6b18fe92e9c11e928
Christopher De Cairos <chris.decairos@gmail.com>
2012-10-16 11:35:11 -0400

    [#2540] Make Unit test runner work in Safari
    [#2540] Drop test support through apache - Node testing only

--- test/butter.testrunner.js
+++ test/butter.testrunner.js
@@ -21,16 +21,7 @@
         testList = [],
         userAgent = id( "qunit-userAgent" ),
         firstTest = true,
-        testBase;
-
-    // Test base needs to be configured differently depending on if someone is running any of the test runners with Node or
-    // with Apache. When using Apache the assumption is made that their repo was named butter
-    if ( location.href.indexOf( "butter" ) ) {
-      testBase = location.href.substring( 0, location.href.indexOf( "butter" ) + 6 ) + "/";
-    }
-    else {
-      testBase = location.protocol + "//" + location.hostname + ( location.port ? ":" + location.port : "" );
-    }
+        testBase = location.protocol + "//" + location.hostname + ( location.port ? ":" + location.port : "" ) + "/";
 
     if ( userAgent ) {
       userAgent.innerHTML = navigator.userAgent;
