00e8a10687a7b652ab01274e97444f9ebb175052
scottdowne <scott.downe@gmail.com>
2013-01-15 10:37:10 -0500

    seq-wrapper

--- templates/assets/plugins/sequencer/popcorn.sequencer.js
+++ templates/assets/plugins/sequencer/popcorn.sequencer.js
@@ -2,13 +2,6 @@
 
 (function (Popcorn) {
 
-  function validateDimension( value, fallback ) {
-    if ( typeof value === "number" ) {
-      return value;
-    }
-    return fallback;
-  }
-
   Popcorn.plugin( 'sequencer', {
     _setup: function( options ) {
       var container = document.createElement( "div" ),
@@ -28,10 +21,10 @@
       container.className = "popcorn-sequencer";
       container.style.position = "absolute";
       container.style.display = "none";
-      container.style.width = validateDimension( options.width, "100" ) + "%";
-      container.style.height = validateDimension( options.height, "100" ) + "%";
-      container.style.top = validateDimension( options.top, "0" ) + "%";
-      container.style.left = validateDimension( options.left, "0" ) + "%";
+      container.style.width = ( options.width || "100" ) + "%";
+      container.style.height = ( options.height || "100" ) + "%";
+      container.style.top = ( options.top || "0" ) + "%";
+      container.style.left = ( options.left || "0" ) + "%";
       mouseDiv.style.position = "absolute";
       mouseDiv.style.width = "100%";
       mouseDiv.style.height = "100%";
@@ -43,7 +36,7 @@
         container.appendChild( mouseDiv );
       }
 
-      options.readyEvent = function() {
+      /*options.readyEvent = function() {
         if ( !options.ready ) {
           var targetTime = that.currentTime() - options.start + ( +options.from );
           options.ready = true;
@@ -55,22 +48,22 @@
             options.p.play();
           }
         }
-      };
+      };*/
 
       if ( options.source ) {
         options.p = Popcorn.smart( container, options.source, {frameAnimation: true} );
         options.p.media.style.width = "100%";
         options.p.media.style.height = "100%";
-        container.style.width = validateDimension( options.width, "100" ) + "%";
-        container.style.height = validateDimension( options.height, "100" ) + "%";
-        if ( options.p.media.readyState >= 4 ) {
+        container.style.width = ( options.width || "100" ) + "%";
+        container.style.height = ( options.height || "100" ) + "%";
+        /*if ( options.p.media.readyState >= 4 ) {
           options.readyEvent();
         } else {
           options.p.on( "canplaythrough", options.readyEvent );
-        }
+        }*/
       }
 
-      options._playEvent = function() {
+      /*options._playEvent = function() {
         options.p.play( that.currentTime() - options.start + (+options.from) );
       };
 
@@ -91,13 +84,13 @@
           }
           options.p.currentTime( that.currentTime() - options.start + (+options.from) );
         }
-      };
+      };*/
     },
     _teardown: function( options ) {
 //      options._target.removeChild( options._container );
     },
     start: function( event, options ) {
-      var targetTime;
+      /*var targetTime;
       this.on( "play", options._playEvent );
       this.on( "pause", options._pauseEvent );
   		this.on( "seeking", options._seekingEvent );
@@ -111,17 +104,17 @@
           options.p.play();
         }
       }
-      options._container.style.display = "block";
+      options._container.style.display = "block";*/
     },
     end: function( event, options ) {
-      this.off( "play", options._playEvent );
+      /*this.off( "play", options._playEvent );
       this.off( "pause", options._pauseEvent );
   		this.off( "seeking", options._seekingEvent );
 			this.off( "seeked", options._seekedEvent );
       if ( options.ready ) {
         options.p.pause();
       }
-      options._container.style.display = "none";
+      options._container.style.display = "none";*/
     },
     manifest: {
       about: {},
