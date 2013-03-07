53ddfb140dbf98284f769e63579e95ecb373d050
Christopher De Cairos <chris.decairos@gmail.com>
2013-03-07 12:08:39 -0500

    Bug #3290 - Flash version detection and warning layout. Only appears if flash is used.

--- public/src/butter.js
+++ public/src/butter.js
@@ -25,8 +25,6 @@ window.Butter = {
     return;
   }
 
-  var WARNING_WAIT_TIME = 500;
-
   var ACCEPTED_UA_LIST = {
     "Chrome": 17,
     "Firefox": 10,
@@ -44,7 +42,13 @@ window.Butter = {
     "Firefox OS",
     // For BB Playbook
     "RIM Tablet OS"
-  ];
+  ],
+
+  UA_WARNING_TEXT = "Your web browser may lack some functionality expected" +
+    " by Popcorn Maker to function properly. Please upgrade your browser or" +
+    " <a href=\"https://webmademovies.lighthouseapp.com/projects/65733-popcorn-maker\">" +
+    "file a bug</a> to find out why your browser isn't fully supported. Click " +
+    "<a href=\"#\" class=\"close-button\">here</a> to remove this warning.";
 
   var require = requirejs.config({
     baseUrl: "/src"
@@ -57,7 +61,7 @@ window.Butter = {
             "./modules", "./dependencies", "./dialogs",
             "dialog/dialog", "editor/editor", "ui/ui",
             "util/xhr", "util/lang", "util/tutorial",
-            "text!default-config.json", "text!layouts/ua-warning.html",
+            "util/warn", "text!default-config.json",
             "ui/widget/tooltip", "crashreporter", "core/project",
             "../external/ua-parser/ua-parser"
           ],
@@ -67,7 +71,7 @@ window.Butter = {
             Modules, Dependencies, Dialogs,
             Dialog, Editor, UI,
             XHR, Lang, Tutorial,
-            DEFAULT_CONFIG_JSON, UA_WARNING_LAYOUT,
+            Warn, DEFAULT_CONFIG_JSON,
             ToolTip, CrashReporter, Project,
             UAParser
           ){
@@ -77,17 +81,6 @@ window.Butter = {
     var Butter = {};
 
     Butter.ToolTip = ToolTip;
-
-    Butter.showUAWarning = function() {
-      var uaWarningDiv = Lang.domFragment( UA_WARNING_LAYOUT, ".butter-ua-warning" );
-      document.body.appendChild( uaWarningDiv );
-      setTimeout(function() {
-        uaWarningDiv.classList.add( "slide-out" );
-      }, WARNING_WAIT_TIME );
-      uaWarningDiv.getElementsByClassName( "close-button" )[ 0 ].onclick = function () {
-        document.body.removeChild( uaWarningDiv );
-      };
-    };
 
     Butter.init = function( butterOptions ) {
 
@@ -109,7 +102,7 @@ window.Butter = {
       }
 
       if ( !acceptedUA ) {
-        Butter.showUAWarning();
+        Warn.showWarning( UA_WARNING_TEXT );
       }
 
       butterOptions = butterOptions || {};
