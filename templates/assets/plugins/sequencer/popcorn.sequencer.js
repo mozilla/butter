8ceb3ea0e5bdb8eff0770ff1211474e9f21a9691
scottdowne <scott.downe@gmail.com>
2013-03-07 05:10:18 -0500

    [#3322] Do not pause when buffering on start and end. Wait for next onprogress.

--- templates/assets/plugins/sequencer/popcorn.sequencer.js
+++ templates/assets/plugins/sequencer/popcorn.sequencer.js
@@ -91,7 +91,6 @@
         options._container.style.top = ( options.top || "0" ) + "%";
         options._container.style.left = ( options.left || "0" ) + "%";
         _this.on( "volumechange", options._volumeEvent );
-        options._clip.on( "progress", options._onProgress );
         if ( options.active ) {
           options._startEvent();
         }
@@ -159,6 +158,7 @@
         _this.off( "play", options._playEvent );
         _this.off( "pause", options._pauseEvent );
         _this.off( "seeked", options._onSeeked );
+        options._clip.off( "progress", options._onProgress );
       };
 
       options.addSource = function() {
@@ -256,6 +256,9 @@
             _this.on( "play", options._playEvent );
             _this.on( "pause", options._pauseEvent );
             _this.on( "seeked", options._onSeeked );
+            // Setup on progress after initial load.
+            // This way if an initial load never happens, we never pause.
+            options._clip.on( "progress", options._onProgress );
             options.hideLoading();
             if ( !options.hidden && options.active ) {
               options._container.style.zIndex = +options.zindex;
