e15d04528d720e580432c0953c856f9ee55d59fa
Matthew Schranz <schranz.m@gmail.com>
2013-04-02 13:03:59 -0400

    Working with silly setTimeout usage when tested in embed layout outside of server

--- public/src/embed.js
+++ public/src/embed.js
@@ -266,157 +266,165 @@ function init() {
       "popcorn"
     ],
     function( URI, Controls, TextboxWrapper ) {
-      // cornfield writes out the Popcorn initialization code as popcornDataFn()
-      window.popcornDataFn();
-      /**
-       * Expose Butter so we can get version info out of the iframe doc's embed.
-       * This "butter" is never meant to live in a page with the full "butter".
-       * We warn then remove if this happens.
-       **/
-      var Butter = {
-            version: "Butter-Embed-@VERSION@"
-          },
-          popcorn = Popcorn.byId( "Butter-Generated" ),
-          config,
-          qs = URI.parse( window.location.href ).queryKey,
-          container = document.querySelectorAll( ".container" )[ 0 ];
+      
+      function initEmbed() {
 
-      /**
-       * the embed can be configured via the query string:
-       *   autohide   = 1{default}|0    automatically hide the controls once playing begins
-       *   autoplay   = 1|{default}0    automatically play the video on load
-       *   controls   = 1{default}|0    display controls
-       *   start      = {integer 0-end} time to start playing (default=0)
-       *   end        = {integer 0-end} time to end playing (default={end})
-       *   fullscreen = 1{default}|0    whether to allow fullscreen mode (e.g., hide/show button)
-       *   loop       = 1|0{default}    whether to loop when hitting the end
-       *   showinfo   = 1{default}|0    whether to show video title, author, etc. before playing
-       **/
-      config = {
-        autohide: qs.autohide === "1" ? true : false,
-        autoplay: qs.autoplay === "1" ? true : false,
-        controls: qs.controls === "0" ? false : true,
-        start: qs.start|0,
-        end: qs.end|0,
-        fullscreen: qs.fullscreen === "0" ? false : (function( document ) {
-          // Check for prefixed/unprefixed Fullscreen API support
-          if ( "fullScreenElement" in document ) {
-            return true;
-          }
+        if ( !window.pop ) {
+          setTimeout( initEmbed, 50 );
+        } else {
+          /**
+           * Expose Butter so we can get version info out of the iframe doc's embed.
+           * This "butter" is never meant to live in a page with the full "butter".
+           * We warn then remove if this happens.
+           **/
+          var Butter = {
+                version: "Butter-Embed-@VERSION@"
+              },
+              popcorn = Popcorn.byId( "Butter-Generated" ),
+              config,
+              qs = URI.parse( window.location.href ).queryKey,
+              container = document.querySelectorAll( ".container" )[ 0 ];
 
-          var pre = "khtml o ms webkit moz".split( " " ),
-              i = pre.length,
-              prefix;
+          /**
+           * the embed can be configured via the query string:
+           *   autohide   = 1{default}|0    automatically hide the controls once playing begins
+           *   autoplay   = 1|{default}0    automatically play the video on load
+           *   controls   = 1{default}|0    display controls
+           *   start      = {integer 0-end} time to start playing (default=0)
+           *   end        = {integer 0-end} time to end playing (default={end})
+           *   fullscreen = 1{default}|0    whether to allow fullscreen mode (e.g., hide/show button)
+           *   loop       = 1|0{default}    whether to loop when hitting the end
+           *   showinfo   = 1{default}|0    whether to show video title, author, etc. before playing
+           **/
+          config = {
+            autohide: qs.autohide === "1" ? true : false,
+            autoplay: qs.autoplay === "1" ? true : false,
+            controls: qs.controls === "0" ? false : true,
+            start: qs.start|0,
+            end: qs.end|0,
+            fullscreen: qs.fullscreen === "0" ? false : (function( document ) {
+              // Check for prefixed/unprefixed Fullscreen API support
+              if ( "fullScreenElement" in document ) {
+                return true;
+              }
 
-          while ( i-- ) {
-            prefix = pre[ i ];
-            if ( (prefix + "FullscreenElement" ) in document ) {
-              return true;
+              var pre = "khtml o ms webkit moz".split( " " ),
+                  i = pre.length,
+                  prefix;
+
+              while ( i-- ) {
+                prefix = pre[ i ];
+                if ( (prefix + "FullscreenElement" ) in document ) {
+                  return true;
+                }
+              }
+              return false;
+            }( document )),
+            loop: qs.loop === "1" ? true : false,
+            branding: qs.branding === "0" ? false : true,
+            showinfo: qs.showinfo === "0" ? false : true
+          };
+
+          // Always show controls.  See #2284 and #2298 on supporting
+          // options.controls, options.autohide.
+          popcorn.controls( true );
+          Controls.create( "controls", popcorn, {
+            onShareClick: function() {
+              shareClick( popcorn );
+            },
+            onRemixClick: function() {
+              remixClick( popcorn );
+            },
+            onFullscreenClick: function() {
+              fullscreenClick();
             }
+          });
+
+          // Setup UI based on config options
+          if ( !config.showinfo ) {
+            var embedInfo = document.getElementById( "embed-info" );
+            embedInfo.parentNode.removeChild( embedInfo );
           }
-          return false;
-        }( document )),
-        loop: qs.loop === "1" ? true : false,
-        branding: qs.branding === "0" ? false : true,
-        showinfo: qs.showinfo === "0" ? false : true
-      };
+          if ( config.loop ) {
+            popcorn.loop( true );
+          }
 
-      // Always show controls.  See #2284 and #2298 on supporting
-      // options.controls, options.autohide.
-      popcorn.controls( true );
-      Controls.create( "controls", popcorn, {
-        onShareClick: function() {
-          shareClick( popcorn );
-        },
-        onRemixClick: function() {
-          remixClick( popcorn );
-        },
-        onFullscreenClick: function() {
-          fullscreenClick();
-        }
-      });
+          // Some config options want the video to be ready before we do anything.
+          function onLoad() {
+            var start = config.start,
+                end = config.end;
 
-      // Setup UI based on config options
-      if ( !config.showinfo ) {
-        var embedInfo = document.getElementById( "embed-info" );
-        embedInfo.parentNode.removeChild( embedInfo );
-      }
-      if ( config.loop ) {
-        popcorn.loop( true );
-      }
+            if ( config.fullscreen ) {
+              // dispatch an event to let the controls know we want to setup a click listener for the fullscreen button
+              popcorn.emit( "butter-fullscreen-allowed", container );
+            }
 
-      // Some config options want the video to be ready before we do anything.
-      function onLoad() {
-        var start = config.start,
-            end = config.end;
+            popcorn.off( "load", onLoad );
 
-        if ( config.fullscreen ) {
-          // dispatch an event to let the controls know we want to setup a click listener for the fullscreen button
-          popcorn.emit( "butter-fullscreen-allowed", container );
-        }
-
-        popcorn.off( "load", onLoad );
-
-        // update the currentTime to the embed options start value
-        // this is needed for mobile devices as attempting to listen for `canplay` or similar events
-        // that let us know it is safe to update the current time seem to be futile
-        function timeupdate() {
-          popcorn.currentTime( start );
-          popcorn.off( "timeupdate", timeupdate );
-        }
-        // See if we should start playing at a time other than 0.
-        // We combine this logic with autoplay, since you either
-        // seek+play or play or neither.
-        if ( start > 0 && start < popcorn.duration() ) {
-          popcorn.on( "seeked", function onSeeked() {
-            popcorn.off( "seeked", onSeeked );
-            if ( config.autoplay ) {
+            // update the currentTime to the embed options start value
+            // this is needed for mobile devices as attempting to listen for `canplay` or similar events
+            // that let us know it is safe to update the current time seem to be futile
+            function timeupdate() {
+              popcorn.currentTime( start );
+              popcorn.off( "timeupdate", timeupdate );
+            }
+            // See if we should start playing at a time other than 0.
+            // We combine this logic with autoplay, since you either
+            // seek+play or play or neither.
+            if ( start > 0 && start < popcorn.duration() ) {
+              popcorn.on( "seeked", function onSeeked() {
+                popcorn.off( "seeked", onSeeked );
+                if ( config.autoplay ) {
+                  popcorn.play();
+                }
+              });
+              popcorn.on( "timeupdate", timeupdate );
+            } else if ( config.autoplay ) {
               popcorn.play();
             }
-          });
-          popcorn.on( "timeupdate", timeupdate );
-        } else if ( config.autoplay ) {
-          popcorn.play();
+
+            // See if we should pause at some time other than duration.
+            if ( end > 0 && end > start && end <= popcorn.duration() ) {
+              popcorn.cue( end, function() {
+                popcorn.pause();
+                popcorn.emit( "ended" );
+              });
+            }
+          }
+
+          // Either the video is ready, or we need to wait.
+          if ( popcorn.readyState() >= 1 ) {
+            onLoad();
+          } else {
+            popcorn.media.addEventListener( "canplay", onLoad );
+          }
+
+          if ( config.branding ) {
+            setupClickHandlers( popcorn, config );
+            setupEventHandlers( popcorn, config );
+
+            // Wrap textboxes so they click-to-highlight and are readonly
+            TextboxWrapper.applyTo( $( "#share-url" ), { readOnly: true } );
+            TextboxWrapper.applyTo( $( "#share-iframe" ), { readOnly: true } );
+
+            // Write out the iframe HTML necessary to embed this
+            $( "#share-iframe" ).value = buildIFrameHTML();
+
+            // Get the page's canonical URL and put in share URL
+            $( "#share-url" ).value = getCanonicalURL();
+          }
+
+          setupAttribution( popcorn );
+
+          if ( window.Butter && console && console.warn ) {
+            console.warn( "Butter Warning: page already contains Butter, removing." );
+            delete window.Butter;
+          }
+          window.Butter = Butter;
         }
-
-        // See if we should pause at some time other than duration.
-        if ( end > 0 && end > start && end <= popcorn.duration() ) {
-          popcorn.cue( end, function() {
-            popcorn.pause();
-            popcorn.emit( "ended" );
-          });
-        }
       }
 
-      // Either the video is ready, or we need to wait.
-      if ( popcorn.readyState() >= 1 ) {
-        onLoad();
-      } else {
-        popcorn.media.addEventListener( "canplay", onLoad );
-      }
-
-      if ( config.branding ) {
-        setupClickHandlers( popcorn, config );
-        setupEventHandlers( popcorn, config );
-
-        // Wrap textboxes so they click-to-highlight and are readonly
-        TextboxWrapper.applyTo( $( "#share-url" ), { readOnly: true } );
-        TextboxWrapper.applyTo( $( "#share-iframe" ), { readOnly: true } );
-
-        // Write out the iframe HTML necessary to embed this
-        $( "#share-iframe" ).value = buildIFrameHTML();
-
-        // Get the page's canonical URL and put in share URL
-        $( "#share-url" ).value = getCanonicalURL();
-      }
-
-      setupAttribution( popcorn );
-
-      if ( window.Butter && console && console.warn ) {
-        console.warn( "Butter Warning: page already contains Butter, removing." );
-        delete window.Butter;
-      }
-      window.Butter = Butter;
+      initEmbed();
     }
   );
 
