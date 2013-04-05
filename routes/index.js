59f32142349402e92adec61b8bb222bd69dd7e86
Jon Buckley <jon@jbuckley.ca>
2013-04-05 12:20:21 -0400

    Bug #858623 - Add CORS headers to public API endpoints

--- routes/index.js
+++ routes/index.js
@@ -47,6 +47,7 @@ module.exports = function routesCtor( app, Project, filter, sanitizer,
 
   app.get( '/api/projects/recentlyCreated/:limit?',
     filter.isStorageAvailable,
+    filter.crossOriginAccessible,
     function( req, res ) {
       Project.findRecentlyCreated( { limit: req.params.limit }, function( err, projects ) {
         if ( err ) {
@@ -58,6 +59,7 @@ module.exports = function routesCtor( app, Project, filter, sanitizer,
 
   app.get( '/api/projects/recentlyUpdated/:limit?',
     filter.isStorageAvailable,
+    filter.crossOriginAccessible,
     function( req, res ) {
       Project.findRecentlyUpdated( { limit: req.params.limit }, function( err, projects ) {
         if ( err ) {
@@ -69,6 +71,7 @@ module.exports = function routesCtor( app, Project, filter, sanitizer,
 
   app.get( '/api/project/:id/remixes',
     filter.isStorageAvailable,
+    filter.crossOriginAccessible,
     function( req, res ) {
       Project.findRemixes( { id: req.params.id }, function( err, projects ) {
         if ( err ) {
