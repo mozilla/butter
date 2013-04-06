a05c178b67013c984005fd4ca96bd6e671294c90
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2013-04-06 18:02:58 -0400

    Bug 858950 - Create a Recently Remixed end point api

--- routes/index.js
+++ routes/index.js
@@ -31,9 +31,8 @@ module.exports = function routesCtor( app, Project, filter, sanitizer,
 
   // Strip away project data, email, etc.
   function pruneSearchResults( results ) {
-    var pruned = [];
-    results.forEach( function( result ) {
-      pruned.push({
+    return results.map( function( result ) {
+      return {
         id: result.id,
         name: result.name,
         author: result.author,
@@ -43,33 +42,30 @@ module.exports = function routesCtor( app, Project, filter, sanitizer,
         // Add URLs for embed, embed shell
         publishUrl: utils.generatePublishUrl( result.id ),
         iframeUrl: utils.generateIframeUrl( result.id )
-      });
+      };
     });
-    return pruned;
   }
 
-  app.get( '/api/projects/recentlyCreated/:limit?',
-    filter.isStorageAvailable,
-    filter.crossOriginAccessible,
-    function( req, res ) {
-      Project.findRecentlyCreated( { limit: req.params.limit }, function( err, projects ) {
-        if ( err ) {
-          res.jsonp( { error: err }, 500 );
-        }
-        res.jsonp( { error: 'okay', results: pruneSearchResults( projects ) } );
-      });
-  });
+  // Setup common recently* API routes--code is identical, save the name.
+  [ 'recentlyCreated',
+    'recentlyUpdated',
+    'recentlyRemixed'
+  ].forEach( function( name ) {
+    var endPoint = '/api/projects/' + name + '/:limit?',
+        projectMethod = 'find' + name[ 0 ].toUpperCase() + name.slice( 1 );
 
-  app.get( '/api/projects/recentlyUpdated/:limit?',
-    filter.isStorageAvailable,
-    filter.crossOriginAccessible,
-    function( req, res ) {
-      Project.findRecentlyUpdated( { limit: req.params.limit }, function( err, projects ) {
-        if ( err ) {
-          res.jsonp( { error: err }, 500 );
-        }
-        res.jsonp( { error: 'okay', results: pruneSearchResults( projects ) } );
-      });
+    app.get( endPoint,
+      filter.isStorageAvailable,
+      filter.crossOriginAccessible,
+      function( req, res ) {
+        Project[ projectMethod ]( { limit: req.params.limit }, function( err, projects ) {
+          if ( err ) {
+            res.jsonp( { error: err }, 500 );
+          }
+          res.jsonp( { status: 'okay', results: pruneSearchResults( projects ) } );
+        });
+      }
+    );
   });
 
   app.get( '/api/project/:id/remixes',
