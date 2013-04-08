ae6553fd80e0930c7ab05a1676206497420a2764
scottdowne <scott.downe@gmail.com>
2013-04-08 07:12:27 -0400

    [#3333] Trying min width and hegith for clips at 10 percent.

--- public/templates/assets/editors/sequencer/sequencer.js
+++ public/templates/assets/editors/sequencer/sequencer.js
@@ -12,7 +12,9 @@ EditorHelper.addPlugin( "sequencer", function( trackEvent ) {
   if ( window.jQuery ) {
     window.EditorHelper.draggable( trackEvent, _container, _target );
     window.EditorHelper.resizable( trackEvent, _container, _target, {
-      handlePositions: "e, se, s, sw, w, n, ne"
+      handlePositions: "e, se, s, sw, w, n, ne",
+      minWidth: 10,
+      minHeight: 10
     });
   }
 });
